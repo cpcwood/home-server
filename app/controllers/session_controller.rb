946503ef4a5e086232e29e1bc919e189b185b843
cpcwood <cpcwood@outlook.com>
2020-04-28 22:46:46 +0100

    admin login functionality added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,16 @@
 class SessionController < ApplicationController
   def login; end
+
+  def new
+    # check credentials
+    user = User.find_by(email: params[:user])
+    user = User.find_by(username: params[:user]) unless user
+    if user && user.authenticate(params[:password])
+      reset_session #reduce risk of session fixation
+      session[:user_id] = user.id
+      redirect_to :admin, notice: "#{user.username} welcome back to your home-server!"
+    else
+      redirect_to :login, alert: "User not found"
+    end
+  end
 end
