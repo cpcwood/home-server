bb684c0f4070c7bd62670203f09fe29bfe032255
cpcwood <cpcwood@outlook.com>
2020-04-28 22:50:38 +0100

    rails protect from forgery, assign user method added to application controller to reset session if user_id is not found, stops errors on reset of database but not cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :assign_user
+
+  private
+
+  def assign_user
+    begin
+      @user = User.find(session[:user_id]) if session[:user_id]
+    rescue
+      reset_session
+      redirect_to('/')
+    end
+  end
 end
