ca33f491054f6dcd7c50fe8428ba37ddfd90f79f
cpcwood <cpcwood@outlook.com>
2020-04-28 23:05:40 +0100

    unauthorised users cannot access admin page, old user sessions are reset by application controller

--- spec/requests/admin_request_spec.rb
+++ spec/requests/admin_request_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Admins', type: :request do
-  it 'renders admin landing page' do
+  it 'renders redirects to homepage if user not logged in' do
     get '/admin'
-    expect(response).to render_template(:index)
+    expect(response).to redirect_to('/')
   end
 end
