e53fcafcc92196c5f7eb7a5263548411d3aab438
cpcwood <cpcwood@outlook.com>
2020-05-01 21:56:19 +0100

    remove redundant spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def assign_user
-    @user = User.find(session[:user_id]) if session[:user_id]
-  rescue StandardError
-    reset_session
-    redirect_to('/')
+    @user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
