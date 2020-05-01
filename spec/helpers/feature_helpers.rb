737991e67a6e70b26b013491287f088989486c5d
cpcwood <cpcwood@outlook.com>
2020-05-01 22:07:51 +0100

    fix webmock for CI

--- spec/helpers/feature_helpers.rb
+++ spec/helpers/feature_helpers.rb
@@ -3,14 +3,6 @@ require 'twilio-ruby'
 
 def login
   stub_request(:post, 'https://www.google.com/recaptcha/api/siteverify?response&secret=test')
-    .with(
-      headers: {
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Content-Length' => '0',
-        'User-Agent' => 'Faraday v1.0.0'
-      }
-    )
     .to_return(status: 200, body: '{"success": true}', headers: {})
   block_twilio_verification_requests
   block_twilio_verification_checks
