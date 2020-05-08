f6ced6ecc5e16adfea8c49ccd0d60dd9e30890c2
cpcwood <cpcwood@outlook.com>
2020-05-08 21:07:40 +0100

    secure password reset token added to user if submitted forgotten password email matches a user record

--- spec/feature/password_reset_spec.rb
+++ spec/feature/password_reset_spec.rb
@@ -4,7 +4,7 @@ feature 'Password Reset' do
       .to_return(status: 200, body: '{"success": true}', headers: {})
     visit('/login')
     click_on('Forgotten Password')
-    fill_in('user', with: 'admin')
+    fill_in('email', with: 'admin@example.com')
     click_button('Request Password Reset')
     expect(page).to have_content('If the submitted email is associated with an account, a password reset link will be sent')
     have_current_path('/login')
