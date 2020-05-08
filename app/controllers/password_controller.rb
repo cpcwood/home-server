7fa53053e46b6697e5f619d7cb6ba28cff5ad5c1
cpcwood <cpcwood@outlook.com>
2020-05-08 19:41:37 +0100

    forgotton password post route added and redirects back to login with default message, feature test for clicking password link added

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,4 +2,8 @@ class PasswordController < ApplicationController
   def forgotten_password
     render :forgotten_password
   end
+
+  def send_reset_link
+    redirect_to(:login, notice: 'If the submitted email is associated with an account, a password reset link will be sent')
+  end
 end
