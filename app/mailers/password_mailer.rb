0d9c8620b972dd238b87f66508cda82f2a0a933e
cpcwood <cpcwood@outlook.com>
2020-05-10 20:20:16 +0100

    password_reset_email added to PasswordMailer and renders the receivers email correctly

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -3,5 +3,8 @@ class PasswordMailer < ApplicationMailer
     # user deleted before email could be sent
   end
 
-  def password_reset_email; end
+  def password_reset_email
+    @user = params[:user]
+    mail(to: @user.email)
+  end
 end
--- spec/mailers/password_mailer_spec.rb
+++ spec/mailers/password_mailer_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PasswordMailer, type: :mailer do
   describe '#password_reset_email' do
+    let(:mail) { PasswordMailer.with(user: @test_user).password_reset_email }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([@test_user.email])
+    end
   end
 end
