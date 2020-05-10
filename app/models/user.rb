780f23965a42b3a83e1abfd807387664aa60f5a3
cpcwood <cpcwood@outlook.com>
2020-05-10 20:56:15 +0100

    #password_reset_email renders reset link with user token, User model no longer encrypts tokens since they expire quickly and are required to be URL safe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
-require 'bcrypt'
-
 class User < ApplicationRecord
   has_secure_password
 
   def send_password_reset_email!
-    self.password_reset_token = generate_hashed_token
-    self.password_reset_expiry = Time.zone.now + 1.hour
+    generate_hashed_token
     PasswordMailer.with(user: self).password_reset_email.deliver_now
   end
 
   private
 
   def generate_hashed_token
-    BCrypt::Password.create(SecureRandom.urlsafe_base64(32))
+    self.password_reset_token = SecureRandom.urlsafe_base64(32)
+    self.password_reset_expiry = Time.zone.now + 1.hour
   end
 end
