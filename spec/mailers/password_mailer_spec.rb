b4029f68c84e6b237047b39b13adcd100093c9a4
cpcwood <cpcwood@outlook.com>
2020-05-10 20:23:54 +0100

    #password_reset_email renders from email

--- spec/mailers/password_mailer_spec.rb
+++ spec/mailers/password_mailer_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe PasswordMailer, type: :mailer do
       expect(mail.to).to eql([@test_user.email])
     end
 
+    it 'renders the from email correctly' do
+      expect(mail.from).to eql([Rails.application.credentials.email[:default_email]])
+    end
+
     it 'renders the subject' do
       expect(mail.subject).to eql("Password Reset: #{@test_user.email}")
     end
