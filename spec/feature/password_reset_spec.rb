fe8031bac543865ce919b23e90ba2be6de1d85db
cpcwood <cpcwood@outlook.com>
2020-05-12 21:34:06 +0100

    feature test for password reset features

--- spec/feature/password_reset_spec.rb
+++ spec/feature/password_reset_spec.rb
@@ -1,3 +1,6 @@
+require 'helpers/session_helper'
+require 'twilio-ruby'
+
 feature 'Password Reset' do
   scenario 'Filling in password reset form' do
     stub_request(:post, 'https://www.google.com/recaptcha/api/siteverify?response&secret=test')
@@ -9,6 +12,35 @@ feature 'Password Reset' do
     expect(page).to have_content('If the submitted email is associated with an account, a password reset link will be sent')
     expect(current_path).to eq('/login')
     expect(PasswordResetJob).to have_been_enqueued.exactly(:once)
-    p @test_user.password_reset_token
+  end
+  
+  scenario 'Submitting new password' do
+    # Reset password
+    @test_user.send_password_reset_email!
+    reset_token = @test_user.password_reset_token
+    visit("/reset-password?reset_token=#{reset_token}")
+    fill_in('password', with: 'new_password')
+    fill_in('password_confirmation', with: 'new_password')
+    click_button('Reset Password')
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('Password updated')
+
+    # Attempt to reset password again with same token
+    visit("/reset-password?reset_token=#{reset_token}")
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('Password reset token expired')
+
+    # Login with new password
+    stub_request(:post, 'https://www.google.com/recaptcha/api/siteverify?response&secret=test')
+    .to_return(status: 200, body: '{"success": true}', headers: {})
+    block_twilio_verification_checks
+    verification_double = double('verification', status: 'approved')
+    allow_any_instance_of(Twilio::REST::Verify::V2::ServiceContext::VerificationCheckList).to receive(:create).and_return(verification_double)
+    fill_in('user', with: 'admin')
+    fill_in('password', with: 'new_password')
+    click_button('Login')
+    fill_in('auth_code', with: '123456')
+    click_button('Login')
+    expect(page).to have_content('admin welcome back to your home-server!')
   end
 end
