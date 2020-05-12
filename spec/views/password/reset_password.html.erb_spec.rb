53d15156d0d03d1af6d23fdbf242d77688d19506
cpcwood <cpcwood@outlook.com>
2020-05-12 19:14:43 +0100

    Reset Password view renders username correctly

---
+++ spec/views/password/reset_password.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'rendering reset password form' do
+  it 'displays username' do
+    assign(:user, @test_user)
+
+    render template: 'password/reset_password.html.erb'
+
+    expect(rendered).to match(/User: admin/)
+  end
+end
