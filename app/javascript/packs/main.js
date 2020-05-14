b007492f62c6dbdb459bb86db5098549db816555
cpcwood <cpcwood@outlook.com>
2020-05-14 18:09:37 +0100

    when clicked navbar makes screen fadeout, clicking screen block closes navmenu, refactor navbar javascript

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,9 +1,7 @@
-import hamburgerAnimation from './navbar/hamburger.js'
 import navMenuClick from './navbar/navMenu.js'
 import touchHover from './touch/touchHover.js'
 
 document.addEventListener('turbolinks:load', function(){
-  hamburgerAnimation()
   navMenuClick()
   touchHover()
 })
