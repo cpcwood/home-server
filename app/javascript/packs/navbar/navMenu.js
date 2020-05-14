84c15c825bcd057a3f49e4cbd12ac02333739301
cpcwood <cpcwood@outlook.com>
2020-05-14 01:56:24 +0100

    fix navbar sizing and animation

--- app/javascript/packs/navbar/navMenu.js
+++ app/javascript/packs/navbar/navMenu.js
@@ -2,13 +2,15 @@ export default function navMenuClick() {
   var hamburger_container = document.querySelector('.hamburger_container')
 
   function openMenu() {
-    this.parentElement.querySelector('.nav_item_container').classList.add('sidebar_open')
+    let navMenu = this.parentElement
+    navMenu.querySelector('.nav_item_container').classList.add('sidebar_open')
     this.removeEventListener('click', openMenu)
     this.addEventListener('click', closeMenu)
   }
 
   function closeMenu() {
-    this.parentElement.querySelector('.nav_item_container').classList.remove('sidebar_open')
+    let navMenu = this.parentElement
+    navMenu.querySelector('.nav_item_container').classList.remove('sidebar_open')
     this.removeEventListener('click', closeMenu)
     this.addEventListener('click', openMenu)
   }
