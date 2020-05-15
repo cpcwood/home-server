52025f42425567e6043636fd203550d05daae613
cpcwood <cpcwood@outlook.com>
2020-05-15 21:43:00 +0100

    email draft complete

--- spec/mailers/previews/password_mailer_preview.rb
+++ spec/mailers/previews/password_mailer_preview.rb
@@ -1,3 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/password_mailer
 class PasswordMailerPreview < ActionMailer::Preview
+  def password_reset
+    @user = User.create(username: 'test', password: 'password', email: 'test@example.com')
+    PasswordMailer.with(user: @user).password_reset_email
+  end
 end
