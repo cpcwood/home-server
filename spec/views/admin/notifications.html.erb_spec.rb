aafd0fb16f5295473130a26a007663c642dc1007
cpcwood <cpcwood@outlook.com>
2020-05-22 16:36:10 +0100

    Admin Notifications placeholder for blog comment  notifications

--- spec/views/admin/notifications.html.erb_spec.rb
+++ spec/views/admin/notifications.html.erb_spec.rb
@@ -9,4 +9,13 @@ describe 'Rendering admin general section' do
 
     expect(rendered).to match(/Contact Emails/)
   end
+
+  it 'Displays new contact emails' do
+    # Placeholder spec
+    assign(:user, @test_user)
+
+    render template: 'admin/notifications.html.erb'
+
+    expect(rendered).to match(/Blog Comments/)
+  end
 end
