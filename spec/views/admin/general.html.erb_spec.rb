e5ac4284a9d49831a29ca9aa614bbfa42abded4e
cpcwood <cpcwood@outlook.com>
2020-05-22 16:04:40 +0100

    Admin General prints out user details

--- spec/views/admin/general.html.erb_spec.rb
+++ spec/views/admin/general.html.erb_spec.rb
@@ -1,22 +1,32 @@
 require 'spec_helper'
 
 describe 'Rendering admin general section' do
-  it 'Displays Last Login Time' do
+  it 'Displays last login time' do
     travel_to Time.zone.local(2020, 04, 19, 15, 20, 00)
     @test_user.update(last_login_time: Time.zone.now)
     assign(:user, @test_user)
 
     render template: 'admin/general.html.erb'
 
-    expect(rendered).to match(/Time and Date: 15:20 19-04-2020/)
+    expect(rendered).to match(%r{Time and Date:</strong> 15:20 19-04-2020})
   end
 
-  it 'Displays Last Login IP Address' do
+  it 'Displays last login ip address' do
     @test_user.update(last_login_ip: '127.0.0.1')
     assign(:user, @test_user)
 
     render template: 'admin/general.html.erb'
 
-    expect(rendered).to match(/IP Address: 127.0.0.1/)
+    expect(rendered).to match(%r{IP Address:</strong> 127.0.0.1})
+  end
+
+  it 'Displays user details' do
+    assign(:user, @test_user)
+
+    render template: 'admin/general.html.erb'
+
+    expect(rendered).to match(%r{Username:</strong> admin})
+    expect(rendered).to match(%r{Email Address:</strong> admin@example.com})
+    expect(rendered).to match(%r{Mobile Number:</strong> \+15005550006})
   end
 end
