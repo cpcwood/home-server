f0944f9f19851cbdb9b8a600ad2be1027a31598a
cpcwood <cpcwood@outlook.com>
2020-05-22 16:39:36 +0100

    Admin Notifications placeholder for say hello notifications

--- spec/views/admin/notifications.html.erb_spec.rb
+++ spec/views/admin/notifications.html.erb_spec.rb
@@ -10,12 +10,30 @@ describe 'Rendering admin general section' do
     expect(rendered).to match(/Contact Emails/)
   end
 
-  it 'Displays new contact emails' do
+  it 'Displays new blog comment notifications' do
     # Placeholder spec
     assign(:user, @test_user)
 
     render template: 'admin/notifications.html.erb'
 
     expect(rendered).to match(/Blog Comments/)
+  end
+
+  it 'Displays new blog comment notifications' do
+    # Placeholder spec
+    assign(:user, @test_user)
+
+    render template: 'admin/notifications.html.erb'
+
+    expect(rendered).to match(/Blog Comments/)
+  end
+
+  it 'Displays new say hello notifications' do
+    # Placeholder spec
+    assign(:user, @test_user)
+
+    render template: 'admin/notifications.html.erb'
+
+    expect(rendered).to match(/Say Hellos/)
   end
 end
