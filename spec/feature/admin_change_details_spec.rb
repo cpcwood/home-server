dcff7edac955c0bf415e59e3326d0fa28507af75
cpcwood <cpcwood@outlook.com>
2020-07-06 19:05:38 +0100

    update username will return validations errors if any

--- spec/feature/admin_change_details_spec.rb
+++ spec/feature/admin_change_details_spec.rb
@@ -1,7 +1,8 @@
 require 'helpers/feature_helpers'
 
 feature 'Admin Change Details' do
-  scenario 'Admin can login and logout' do
+  scenario 'Admin can change username' do
     login_feature
+    # tbc
   end
 end
