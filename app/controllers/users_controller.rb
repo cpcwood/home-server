9f007dcd8afebee46d37cf0aa8132e9716589714
cpcwood <cpcwood@outlook.com>
2020-07-06 21:05:34 +0100

    user change email method completed in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def update
     return redirect_to(admin_user_settings_path, alert: 'Enter current password to update details') unless verify_current_password
     return redirect_to(admin_user_settings_path, alert: @user.errors.values.flatten.last) unless update_username
+    return redirect_to(admin_user_settings_path, alert: @user.errors.values.flatten.last) unless update_email
     redirect_to(admin_user_settings_path, notice: 'User updated!')
   end
 
@@ -32,5 +33,13 @@ class UsersController < ApplicationController
 
   def username_update_params
     params.require(:username).permit(:username, :username_confirmation)
+  end
+
+  def update_email
+    update_section?(email_update_params) ? @user.update(email_update_params) : true
+  end
+
+  def email_update_params
+    params.require(:email).permit(:email, :email_confirmation)
   end
 end
