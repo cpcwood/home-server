0741cacffe5b48bd152dd6f178aef0368af3faf2
cpcwood <cpcwood@outlook.com>
2020-07-06 21:05:34 +0100

    user change email method completed in controller

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,6 +3,13 @@ require 'helpers/session_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'PUT /user.id #update' do
+
+    before(:each) do
+      @blank_username_params = {username: '', username_confirmation: ''}
+      @blank_email_params = {email: '', email_confirmation: ''}
+      @default_current_password_params = { password: @test_user_password }
+    end
+
     it 'Redirects to homepage if user not logged in' do
       put "/users.#{@test_user.id}"
       expect(response.status).to eq(401)
@@ -10,14 +17,30 @@ RSpec.describe 'Users', type: :request do
 
     it 'Original password must be present to update section' do
       login
-      put "/users.#{@test_user.id}", params: { username: { username: 'new_username', username_confirmation: 'new_username' }, current_password: { password: '' }}
+      put "/users.#{@test_user.id}", params: { 
+        username: { 
+          username: 'new_username', 
+          username_confirmation: 'new_username' 
+        }, 
+        email: @blank_email_params,
+        current_password: { 
+          password: '' 
+        }
+      }
       follow_redirect!
       expect(response.body).to include('Enter current password to update details')
     end
 
     it 'Username can be updated' do
       login
-      put "/users.#{@test_user.id}", params: { username: { username: 'new_username', username_confirmation: 'new_username' }, current_password: { password: @test_user_password }}
+      put "/users.#{@test_user.id}", params: {
+        username: { 
+          username: 'new_username',
+          username_confirmation: 'new_username' 
+        },
+        email: @blank_email_params,
+        current_password: @default_current_password_params
+      }
       follow_redirect!
       expect(response.body).to include('User updated!')
       @test_user.reload
@@ -27,17 +50,48 @@ RSpec.describe 'Users', type: :request do
     it 'Username not updated if fields are left empty' do
       original_username = @test_user.username
       login
-      put "/users.#{@test_user.id}", params: { username: { username: '', username_confirmation: '' }, current_password: { password: @test_user_password }}
+      put "/users.#{@test_user.id}", params: {
+        username: {
+          username: '', 
+          username_confirmation: '' 
+        }, 
+        email: @blank_email_params,
+        current_password: @default_current_password_params
+      }
       expect(response).to redirect_to(admin_user_settings_path)
       @test_user.reload
       expect(@test_user.username).to eq(original_username)
     end
 
     it 'Username update validation errors get displayed' do
+      other_params = {current_password: { password: @test_user_password }, email: {}}
       login
-      put "/users.#{@test_user.id}", params: { username: { username: 'new_username', username_confirmation: '' }, current_password: { password: @test_user_password }}
+      put "/users.#{@test_user.id}", params: { 
+        username: { 
+          username: 'new_username', 
+          username_confirmation: '' 
+        }, 
+        email: @blank_email_params,
+        current_password: @default_current_password_params
+      }
       follow_redirect!
       expect(response.body).to include('Usernames do not match')
+    end
+
+    it 'Email can be updated' do
+      login
+      put "/users.#{@test_user.id}", params: { 
+        email: { 
+          email: 'new@example.com', 
+          email_confirmation: 'new@example.com' 
+        }, 
+        username: @blank_username_params,
+        current_password: @default_current_password_params
+      }
+      follow_redirect!
+      expect(response.body).to include('User updated!')
+      @test_user.reload
+      expect(@test_user.email).to eq('new@example.com')
     end
   end
 end
