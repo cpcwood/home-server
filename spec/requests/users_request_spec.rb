b75062ac593f4c777794f7ea87eb3aab9f4e01e0
cpcwood <cpcwood@outlook.com>
2020-07-06 18:09:09 +0100

    use modern lambda notation

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,13 +3,17 @@ require 'helpers/session_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'PUT /user.id #update' do
-    it 'User can update username' do
+    it 'Username can be update' do
       login
-      put "/users.#{@test_user.id}", params: { username: {username: 'new_username', username_confirmation: 'new_username' }}
+      put "/users.#{@test_user.id}", params: { username: { username: 'new_username', username_confirmation: 'new_username' }}
       follow_redirect!
       expect(response.body).to include('User updated')
       @test_user.reload
       expect(@test_user.username).to eq('new_username')
+    end
+
+    it 'Username validation errors displayed' do
+      login
     end
   end
 end
