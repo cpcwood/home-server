e3d7ab62e1a99b0a4ce83a93379800b493d783da
cpcwood <cpcwood@outlook.com>
2020-07-07 15:31:32 +0100

    update rails generators to use RSpec instead of test

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,14 @@ module HomeServer
     config.load_defaults 6.0
     config.middleware.use Rack::Attack
 
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => false
+      g.template_engine :erb
+      g.view_specs      false
+      g.helper_specs    false
+      g.stylesheets     false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
