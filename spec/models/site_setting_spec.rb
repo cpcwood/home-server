fcdad7f50c19b02087795ce764448d546f164e83
cpcwood <cpcwood@outlook.com>
2020-07-07 16:49:40 +0100

    SiteSetting.name validations - accepts correct length names

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -11,9 +11,16 @@ RSpec.describe SiteSetting, type: :model do
     end
 
     it 'Rejects too long' do
-      site_setting.name = str = "0" * 256
+      site_setting.name = '0' * 256
       expect(site_setting).to_not be_valid
       expect(site_setting.errors.messages[:name]).to eq(['Site name cannot be longer than 255 charaters'])
+    end
+
+    it 'Accepts correct length' do
+      site_setting.name = '0'
+      expect(site_setting).to be_valid
+      site_setting.name = '0' * 255
+      expect(site_setting).to be_valid
     end
   end
 end
