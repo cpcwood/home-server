647d9d9fc94c84f6f59eeffb89325f876c7b858f
cpcwood <cpcwood@outlook.com>
2020-07-07 16:46:07 +0100

    SiteSetting.name validation - minimum length

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -1,2 +1,4 @@
 class SiteSetting < ApplicationRecord
+  validates :name,
+            length: { in: 1..255, too_short: 'Site name cannot be blank', too_long: 'Site name cannot be longer than 255 charaters' }
 end
