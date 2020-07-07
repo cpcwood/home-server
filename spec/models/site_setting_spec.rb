d6f7a1c7d32bb97a89d5ed25148e5ef5d796baa6
cpcwood <cpcwood@outlook.com>
2020-07-07 16:46:07 +0100

    SiteSetting.name validation - minimum length

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SiteSetting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:site_setting) { SiteSetting.create(name: 'test_name') }
+
+  describe 'Name validations' do
+    it 'Rejects too short' do
+      site_setting.name = ''
+      expect(site_setting).to_not be_valid
+      expect(site_setting.errors.messages[:name]).to eq(['Site name cannot be blank'])
+    end
+  end
 end
