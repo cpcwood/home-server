17fa97c250510e0a57ecc2b5e39035c5388b28bb
cpcwood <cpcwood@outlook.com>
2020-07-08 17:46:07 +0100

    SiteSettings#update cover image upload added

--- spec/requests/site_settings_request_spec.rb
+++ spec/requests/site_settings_request_spec.rb
@@ -3,7 +3,7 @@ require 'helpers/session_helper'
 
 RSpec.describe 'SiteSettings', type: :request do
   describe 'PUT /admin/site_setting.id #update' do
-    let(:cover_image_path) {Rails.root.join('spec/files/sample_image_low.jpg')}
+    let(:cover_image_path) { Rails.root.join('spec/files/sample_image_low.jpg') }
 
     context 'name' do
       it 'Update sucessful' do
@@ -11,6 +11,9 @@ RSpec.describe 'SiteSettings', type: :request do
         put "/admin/site_settings.#{@site_settings.id}", params: {
           site_setting: {
             name: 'new_site_name'
+          },
+          image_upload: {
+            cover_image: ''
           }
         }
         follow_redirect!
@@ -24,10 +27,32 @@ RSpec.describe 'SiteSettings', type: :request do
         put "/admin/site_settings.#{@site_settings.id}", params: {
           site_setting: {
             name: ''
+          },
+          image_upload: {
+            cover_image: ''
           }
         }
         follow_redirect!
         expect(response.body).to include('Site name cannot be blank')
+      end
+    end
+
+    context 'cover_image' do
+      it 'Update sucessful' do
+        cover_image = fixture_file_upload(cover_image_path, 'image/png')
+        login
+        put "/admin/site_settings.#{@site_settings.id}", params: {
+          site_setting: {
+            name: ''
+          },
+          image_upload: {
+            cover_image: cover_image
+          }
+        }
+        follow_redirect!
+        expect(response.body).to include('Site cover_image updated!')
+        @site_settings.reload
+        expect(@site_settings.cover_image.attached?).to eq(true)
       end
     end
   end
