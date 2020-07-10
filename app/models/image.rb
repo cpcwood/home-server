92338ab59ed958359df20919e008f1107d2e67e9
cpcwood <cpcwood@outlook.com>
2020-07-10 19:58:32 +0100

    SiteSetting.resize_image extracted to Image.resize

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,28 @@
 class Image < ApplicationRecord
+  require 'mini_magick'
+
   belongs_to :site_setting
 
   validates :name,
             length: { in: 1..255, too_short: 'Image name cannot be blank', too_long: 'Image name cannot be longer than 255 charaters' }
+
+  def self.resize(image_path:, x_dim:, y_dim:)
+    expanded_image = expand_image(image_path: image_path, x_dim: x_dim)
+    resized_image = vertical_center_crop_image(image_path: expanded_image.path, y_dim: y_dim)
+    remove_exif_data(resized_image.path)
+  end
+
+  private_class_method def self.expand_image(image_path:, x_dim: nil, y_dim: nil)
+    ImageProcessing::MiniMagick.source(image_path).resize_to_fit(x_dim, y_dim).call
+  end
+
+  private_class_method def self.vertical_center_crop_image(image_path:, x_dim: nil, y_dim: nil)
+    dimensions = MiniMagick::Image.new(image_path).dimensions
+    v_crop_start = (dimensions[1] - y_dim) / 2
+    ImageProcessing::MiniMagick.source(image_path).crop(0, v_crop_start, x_dim, y_dim).call
+  end
+
+  private_class_method def self.remove_exif_data(image_path)
+    ImageProcessing::MiniMagick.source(image_path).strip.call
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Image, type: :model do
   let(:site_setting) { SiteSetting.new }
   let(:image) { Image.new(name: 'test_image', site_setting: site_setting) }
+  let(:image_path_valid) { Rails.root.join('spec/files/sample_image.jpg') }
 
   describe 'Name validations' do
     it 'Rejects too short' do
@@ -22,6 +23,14 @@ RSpec.describe Image, type: :model do
       expect(image).to be_valid
       image.name = '0' * 255
       expect(image).to be_valid
+    end
+  end
+
+  describe '.resize', slow: true do
+    it 'resizes image to target dimensions' do
+      target_dimensions = [2560, 300]
+      processed_image = Image.resize(image_path: image_path_valid, x_dim: target_dimensions[0], y_dim: target_dimensions[1])
+      expect(MiniMagick::Image.new(processed_image.path).dimensions).to eq(target_dimensions)
     end
   end
 end
