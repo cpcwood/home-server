ad0340e7b80d883e5d4b42a83d38452ed7c55c2a
cpcwood <cpcwood@outlook.com>
2020-07-10 20:03:18 +0100

    SiteSetting.image_valid? extracted to Image.is_valid?

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SiteSetting, type: :model do
   let(:site_setting) { @site_settings }
-  let(:image_path_valid) { Rails.root.join('spec/files/sample_image.jpg') }
-  let(:image_path_invalid) { Rails.root.join('spec/files/sample_image_invalid.jpg') }
-  let(:text_file_path) { Rails.root.join('spec/files/sample_text.txt') }
-  let(:image_gif_path) { Rails.root.join('spec/files/sample_image_gif.gif') }
-  let(:image_png_path) { Rails.root.join('spec/files/sample_image_png.png') }
-  let(:image_file_upload) { fixture_file_upload(image_path_valid, 'image/jpg') }
 
   describe 'Name validations' do
     it 'Rejects too short' do
@@ -39,28 +33,6 @@ RSpec.describe SiteSetting, type: :model do
     it 'Update required' do
       new_name = 'new_name'
       expect(site_setting.update_required?('name', new_name)).to eq(true)
-    end
-  end
-
-  describe '.image_valid?', slow: true do
-    it 'image is valid jpeg' do
-      expect(SiteSetting.image_valid?(image_path_valid)).to eq(true)
-    end
-
-    it 'image is invalid' do
-      expect(SiteSetting.image_valid?(image_path_invalid)).to eq(false)
-    end
-
-    it 'not an image' do
-      expect(SiteSetting.image_valid?(text_file_path)).to eq(false)
-    end
-
-    it 'image mime type is png' do
-      expect(SiteSetting.image_valid?(image_png_path)).to eq(true)
-    end
-
-    it 'image mime type is gif' do
-      expect(SiteSetting.image_valid?(image_gif_path)).to eq(false)
     end
   end
 end
