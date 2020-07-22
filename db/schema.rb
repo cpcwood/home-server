3a4fd67a2fa1f9ba6a008cb68170af00079aaeef
cpcwood <cpcwood@outlook.com>
2020-07-22 20:09:13 +0100

    Add typed_header options to SiteSetting model to allow for future customisation of homepage title and subititle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_160315) do
+ActiveRecord::Schema.define(version: 2020_07_22_190353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2020_07_22_160315) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "typed_header_enabled"
+    t.string "header_text"
+    t.string "subtitle_text"
   end
 
   create_table "users", force: :cascade do |t|
