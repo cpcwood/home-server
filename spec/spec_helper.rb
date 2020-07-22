d619eb79406b9b6625c241d2164b8aa43e378b86
cpcwood <cpcwood@outlook.com>
2020-07-22 20:24:40 +0100

    SiteSetting typed_header_enabled validations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
     DatabaseCleaner.start
     @test_user_password = 'Securepass1'
     @test_user = User.create(username: 'admin', email: 'admin@example.com', password: @test_user_password, mobile_number: '+447123456789')
-    @site_settings = SiteSetting.create(name: 'test_name')
+    @site_settings = SiteSetting.create(name: 'test_name', typed_header_enabled: false)
     @header_image = Image.create(site_setting: @site_settings, name: 'header_image', x_dim: 2560, y_dim: 300, image_type: 'header_image')
     @cover_image = Image.create(site_setting: @site_settings, name: 'cover_image', x_dim: 1450, y_dim: 680, image_type: 'cover_image')
   end
