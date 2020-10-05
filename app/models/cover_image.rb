535666693b500db319a65afbfb962bbf43d22029
cpcwood <cpcwood@outlook.com>
2020-10-05 20:47:02 +0100

    admin image upload controllers updated to match new models

--- app/models/cover_image.rb
+++ app/models/cover_image.rb
@@ -1,3 +1,7 @@
 class CoverImage < Image
   belongs_to :site_setting
+
+  def default_image
+    'default_images/default_cover_image.jpg'
+  end
 end
--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -1,3 +1,7 @@
 class ProfileImage < Image
   belongs_to :about
+
+  def default_image
+    'default_images/default_cover_image.jpg'
+  end
 end
