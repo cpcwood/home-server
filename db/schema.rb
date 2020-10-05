0ee0350c136566aedd028a9882b1084733b5a52c
cpcwood <cpcwood@outlook.com>
2020-10-05 16:39:18 +0100

    add abouts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_190353) do
+ActiveRecord::Schema.define(version: 2020_10_05_153032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.string "name"
+    t.text "about_me"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
---
+++ spec/models/about_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe About, type: :model do
+  
+end
