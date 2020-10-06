b255d417fafdd3170599837625bc5a853f0d4d70
cpcwood <cpcwood@outlook.com>
2020-10-06 13:47:48 +0100

    Image model - add description validation for presence and length greater than 1

--- app/models/image.rb
+++ app/models/image.rb
@@ -24,6 +24,10 @@ class Image < ApplicationRecord
             presence: true,
             numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 
+  validates :description,
+            presence: true,
+            length: { minimum: 1 }
+
   def reset_to_default
     image_file.purge
     update(x_loc: DEFAULT_X_LOC)
