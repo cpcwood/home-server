6815a2459c7f0d41a59acb5c8b3b8fc58cc49bab
cpcwood <cpcwood@outlook.com>
2020-10-06 23:19:24 +0100

    PUT /admin/about - sucessful request

--- app/controllers/admin/abouts_controller.rb
+++ app/controllers/admin/abouts_controller.rb
@@ -1,7 +1,47 @@
 module Admin
   class AboutsController < AdminBaseController
-    def edit
+    before_action :assign_abouts
+
+    def edit; end
+
+    def update
+      @notices = []
+      @alerts = []
+      begin
+        update_about
+      rescue StandardError => e
+        @alerts.push('Sorry, something went wrong!')
+        @alerts.push(e.message)
+      end
+      redirect_to(edit_admin_about_path, notice: @notices, alert: @alerts)
+    end
+
+    private
+
+    def assign_abouts
       @about = About.first
+    end
+
+    def attribute_params
+      params.require(:about).permit(
+        :name,
+        :about_me)
+    end
+
+    def profile_image_params
+      params.require(:profile_image).permit(
+        :update,
+        :reset)
+    end
+
+    def update_about
+      if @about.update(attribute_params)
+        changes = @about.previous_changes.keys - ['updated_at']
+        changes.each{ |key| @notices.push("#{key.humanize} updated!") }
+      else
+        @alerts.push(@about.errors.values.flatten.last)
+        @about.reload
+      end
     end
   end
 end
