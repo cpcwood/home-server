d7d3d59f99da8ad419a2724ef3264ea67034941c
cpcwood <cpcwood@outlook.com>
2020-10-06 16:44:01 +0100

    update form styles

--- app/helpers/attachment_helper.rb
+++ app/helpers/attachment_helper.rb
@@ -1,9 +1,8 @@
 module AttachmentHelper
+  # rubocop:disable Lint/SafeNavigationChain
   def image_file_name(image:, default_name: nil, model_name: nil)
-    if image&.image_file.attached?
-      image.image_file.attachment.blob.filename
-    else
-      default_name ? default_name : "default #{"#{model_name} " if model_name}image"
-    end
+    return image.image_file.attachment.blob.filename if image&.image_file.attached?
+    default_name || "default #{"#{model_name} " if model_name}image"
   end
+  # rubocop:enable Lint/SafeNavigationChain
 end
