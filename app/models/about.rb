5be6ebd3b6799fcd95b754ca0be55cf0ed1b0aa4
cpcwood <cpcwood@outlook.com>
2020-10-08 17:55:06 +0100

    About#change_messages - attribute changes

--- app/models/about.rb
+++ app/models/about.rb
@@ -3,6 +3,8 @@ class About < ApplicationRecord
   accepts_nested_attributes_for :profile_image, allow_destroy: true
 
   def change_messages
-    []
+    messages = []
+    messages += (previous_changes.keys - ['updated_at'])
+    messages.map!{ |key| "About #{key.humanize(capitalize: false)} updated!" }
   end
 end
--- spec/models/about_spec.rb
+++ spec/models/about_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe About, type: :model do
   describe '#change_messages' do
-    before(:each) do
-      @about = About.create
+    it 'no changes' do
+      about = About.create
+      about.reload
+      expect(about.change_messages).to eq([])
     end
 
-    it 'no changes' do
-      expect(@about.change_messages).to eq([])
+    it 'attribute changes' do
+      about = About.create
+      about.update(name: 'new name', about_me: 'new description')
+      expect(about.change_messages).to eq(["About name updated!", "About about me updated!"])
     end
   end
 end
