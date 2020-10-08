a0396430b9223c2b585182887e96a67b4e8d9dad
cpcwood <cpcwood@outlook.com>
2020-10-08 18:37:26 +0100

    PUT /admin/site-settings - refactor tests and error handling

--- spec/requests/admin/site_settings_request_spec.rb
+++ spec/requests/admin/site_settings_request_spec.rb
@@ -7,77 +7,63 @@ RSpec.describe 'Request Admin:SiteSettings', type: :request, slow: true do
   end
 
   describe 'GET /admin/site_settings #index' do
-    it 'Renders the index page' do
+    it 'renders the index page' do
       get '/admin/site_settings'
       expect(response).to render_template(:index)
     end
   end
 
   describe 'PUT /admin/site_settings/:id #update' do
-    context 'name' do
-      it 'Update sucessful' do
-        put "/admin/site_settings/#{@site_settings.id}", params: {
-          site_setting: {
-            name: 'new_site_name'
-          }
+    it 'update unsuccessful - validations' do
+      put "/admin/site_settings/#{@site_settings.id}", params: {
+        site_setting: {
+          name: ''
         }
-        follow_redirect!
-        expect(response.body).to include('Name updated!')
-        @site_settings.reload
-        expect(@site_settings.name).to eq('new_site_name')
-      end
-
-      it 'Update fails' do
-        put "/admin/site_settings/#{@site_settings.id}", params: {
-          site_setting: {
-            name: ''
-          }
-        }
-        follow_redirect!
-        expect(response.body).to include('Site name cannot be blank')
-      end
+      }
+      follow_redirect!
+      expect(response.body).to include('Site name cannot be blank')
     end
 
-    context 'header_text' do
-      it 'Update sucessful' do
-        put "/admin/site_settings/#{@site_settings.id}", params: {
-          site_setting: {
-            header_text: 'New header text'
-          }
+    it 'update unsuccessful - general error' do
+      allow_any_instance_of(SiteSetting).to receive(:update).and_raise('general error')
+      put "/admin/site_settings/#{@site_settings.id}", params: {
+        site_setting: {
+          name: ''
         }
-        follow_redirect!
-        expect(response.body).to include('Header text updated!')
-        @site_settings.reload
-        expect(@site_settings.header_text).to eq('New header text')
-      end
+      }
+      follow_redirect!
+      expect(response.body).to include('Sorry, something went wrong!')
+      expect(response.body).to include('general error')
     end
 
-    context 'subtitle_text' do
-      it 'Update sucessful' do
-        put "/admin/site_settings/#{@site_settings.id}", params: {
-          site_setting: {
-            subtitle_text: 'New subtitle text'
-          }
+    it 'single attribute update' do
+      put "/admin/site_settings/#{@site_settings.id}", params: {
+        site_setting: {
+          name: 'new_site_name'
         }
-        follow_redirect!
-        expect(response.body).to include('Subtitle text updated!')
-        @site_settings.reload
-        expect(@site_settings.subtitle_text).to eq('New subtitle text')
-      end
+      }
+      follow_redirect!
+      expect(response.body).to include('Name updated!')
+      @site_settings.reload
+      expect(@site_settings.name).to eq('new_site_name')
     end
 
-    context 'typed_header_enabled' do
-      it 'Update sucessful' do
-        put "/admin/site_settings/#{@site_settings.id}", params: {
-          site_setting: {
-            typed_header_enabled: '1'
-          }
+    it 'multiple attribute update' do
+      put "/admin/site_settings/#{@site_settings.id}", params: {
+        site_setting: {
+          header_text: 'New header text',
+          subtitle_text: 'New subtitle text',
+          typed_header_enabled: '1'
         }
-        follow_redirect!
-        expect(response.body).to include('Typed header enabled updated!')
-        @site_settings.reload
-        expect(@site_settings.typed_header_enabled).to eq(true)
-      end
+      }
+      follow_redirect!
+      expect(response.body).to include('Header text updated!')
+      expect(response.body).to include('Typed header enabled updated!')
+      expect(response.body).to include('Subtitle text updated!')
+      @site_settings.reload
+      expect(@site_settings.header_text).to eq('New header text')
+      expect(@site_settings.subtitle_text).to eq('New subtitle text')
+      expect(@site_settings.typed_header_enabled).to eq(true)
     end
   end
 end
