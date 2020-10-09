cc36957b8221ecb4372d57693d3618d0c3e1f5c2
cpcwood <cpcwood@outlook.com>
2020-10-09 19:11:06 +0100

    TwoFactorAuthService.started? method to check session

--- app/services/two_factor_auth_service.rb
+++ app/services/two_factor_auth_service.rb
@@ -6,5 +6,9 @@ module TwoFactorAuthService
       session[:two_factor_auth_id] = user.id
       true
     end
+
+    def started?(session)
+      !!session[:two_factor_auth_id]
+    end
   end
 end
--- spec/services/two_factor_auth_service_spec.rb
+++ spec/services/two_factor_auth_service_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe TwoFactorAuthService do
+  before(:each) do
+    @session = {}
+  end
   describe '.start' do
     it 'user start two factor auth flow' do
-      mock_session = {}
-      subject.start(mock_session, @test_user)
-      expect(mock_session[:two_factor_auth_id]).to eq(@test_user.id)
+      subject.start(@session, @test_user)
+      expect(@session[:two_factor_auth_id]).to eq(@test_user.id)
+    end
+  end
+
+  describe '.started?' do
+    it 'two factor not started' do
+      expect(subject.started?(@session)).to eq(false)
+    end
+
+    it 'two factor started' do
+      subject.start(@session, @test_user)
+      expect(subject.started?(@session)).to eq(true)
     end
   end
 end
