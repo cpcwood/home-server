438298ae9c112a6b3cb1c5519005be21fe6ff60b
cpcwood <cpcwood@outlook.com>
2020-10-09 19:26:43 +0100

    TwoFactorAuthService.send_auth_code - user has no mobile number

--- app/services/two_factor_auth_service.rb
+++ app/services/two_factor_auth_service.rb
@@ -10,5 +10,10 @@ module TwoFactorAuthService
     def started?(session)
       !!session[:two_factor_auth_id]
     end
+
+    def send_auth_code(session)
+      client_verify_number = User.find(session[:two_factor_auth_id]).mobile_number
+      return false unless client_verify_number
+    end
   end
 end
