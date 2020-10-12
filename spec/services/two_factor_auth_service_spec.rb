2df284e480fd0754f68ab07f9ee57126deea9938
cpcwood <cpcwood@outlook.com>
2020-10-12 12:00:29 +0100

    TwoFactorAuthService.verfiy_auth_code - check user exists

--- spec/services/two_factor_auth_service_spec.rb
+++ spec/services/two_factor_auth_service_spec.rb
@@ -1,60 +1,70 @@
 require 'spec_helper'
 
 describe TwoFactorAuthService do
-  before(:each) do
-    @session = {}
-  end
+  let(:session) { {} }
 
   describe '.start' do
     it 'user start two factor auth flow' do
-      subject.start(@session, @test_user)
-      expect(@session[:two_factor_auth_id]).to eq(@test_user.id)
+      subject.start(session, @test_user)
+      expect(session[:two_factor_auth_id]).to eq(@test_user.id)
     end
   end
 
   describe '.started?' do
     it 'two factor not started' do
-      expect(subject.started?(@session)).to eq(false)
+      expect(subject.started?(session)).to eq(false)
     end
 
     it 'two factor started' do
-      subject.start(@session, @test_user)
-      expect(subject.started?(@session)).to eq(true)
+      subject.start(session, @test_user)
+      expect(subject.started?(session)).to eq(true)
     end
   end
 
   describe '.send_auth_code' do
     before(:each) do
-      subject.start(@session, @test_user)
+      subject.start(session, @test_user)
     end
 
     it 'user does not exist' do
       @test_user.destroy
-      expect(subject.send_auth_code(@session)).to eq(false)
+      expect(subject.send_auth_code(session)).to eq(false)
     end
 
     it 'user has no mobile number' do
       @test_user.mobile_number = nil
       @test_user.save
-      expect(subject.send_auth_code(@session)).to eq(false)
+      expect(subject.send_auth_code(session)).to eq(false)
     end
 
     it 'twilio error' do
       allow(Twilio::REST::Client).to receive(:new).and_raise('error')
-      expect(subject.send_auth_code(@session)).to eq(false)
+      expect(subject.send_auth_code(session)).to eq(false)
     end
 
     it 'successful send request' do
       expect_any_instance_of(Twilio::REST::Verify::V2::ServiceContext::VerificationList).to receive(:create).with(to: @test_user.mobile_number, channel: 'sms')
-      expect(subject.send_auth_code(@session)).to eq(true)
-      expect(@session[:two_factor_auth_code_sent]).to eq(true)
+      expect(subject.send_auth_code(session)).to eq(true)
+      expect(session[:two_factor_auth_code_sent]).to eq(true)
     end
 
     it 'send request already made' do
       allow_any_instance_of(Twilio::REST::Verify::V2::ServiceContext::VerificationList).to receive(:create)
-      subject.send_auth_code(@session)
+      subject.send_auth_code(session)
       expect_any_instance_of(Twilio::REST::Verify::V2::ServiceContext::VerificationList).not_to receive(:create)
-      expect(subject.send_auth_code(@session)).to eq(true)
+      expect(subject.send_auth_code(session)).to eq(true)
+    end
+  end
+
+  describe '.verify_auth_code' do
+    before(:each) do
+      subject.start(session, @test_user)
+    end
+
+    it 'user does not exist' do
+      @test_user.destroy
+      auth_code = 123456
+      expect(subject.verify_auth_code(session, auth_code)).to eq(false)
     end
   end
 end
