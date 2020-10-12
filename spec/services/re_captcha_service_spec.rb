03ca4d8e9d1685ae7c2e46d5e23f8a8f8f3b649b
cpcwood <cpcwood@outlook.com>
2020-10-12 15:15:25 +0100

    ReCaptchaService verify outgoing request using stub

--- spec/services/re_captcha_service_spec.rb
+++ spec/services/re_captcha_service_spec.rb
@@ -7,8 +7,10 @@ describe ReCaptchaService do
 
   describe '.recaptcha_valid?' do
     it 'valid client_response' do
-      expect(Faraday).to receive(:post).with('https://www.google.com/recaptcha/api/siteverify').and_return(post_response_valid)
+      request_stub = stub_request(:post, "https://www.google.com/recaptcha/api/siteverify?response=#{client_response}&secret=#{Rails.application.credentials.recaptcha[:site_secret]}")
+                     .to_return(status: 200, body: '{"success": true}', headers: {})
       expect(subject.recaptcha_valid?(client_response)).to eq(true)
+      expect(request_stub).to have_been_requested.times(1)
     end
 
     it 'invalid client_response' do
