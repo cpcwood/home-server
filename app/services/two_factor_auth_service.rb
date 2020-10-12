12312f26b277cdfd70fe8ead8845c516324e7468
cpcwood <cpcwood@outlook.com>
2020-10-12 12:00:29 +0100

    TwoFactorAuthService.verfiy_auth_code - check user exists

--- app/services/two_factor_auth_service.rb
+++ app/services/two_factor_auth_service.rb
@@ -15,21 +15,32 @@ module TwoFactorAuthService
 
     def send_auth_code(session)
       return true if session[:two_factor_auth_code_sent] == true
-      client_verify_number = User.find_by(id: session[:two_factor_auth_id])&.mobile_number
-      return false unless client_verify_number
+      user_mobile_number = get_user_mobile_number(session)
+      return false unless user_mobile_number
       begin
         Twilio::REST::Client
           .new(Rails.application.credentials.twilio[:account_sid], Rails.application.credentials.twilio[:auth_token])
           .verify
           .services(Rails.application.credentials.twilio[:verify_service_sid])
           .verifications
-          .create(to: client_verify_number, channel: 'sms')
+          .create(to: user_mobile_number, channel: 'sms')
         session[:two_factor_auth_code_sent] = true
         true
       rescue StandardError => e
         logger.error(e)
         false
       end
+    end
+
+    def verify_auth_code(session, auth_code)
+      user_mobile_number = get_user_mobile_number(session)
+      return false unless user_mobile_number
+    end
+
+    private
+
+    def get_user_mobile_number(session)
+      User.find_by(id: session[:two_factor_auth_id])&.mobile_number
     end
   end
 end
