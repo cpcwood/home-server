44e5c0bf6a41dae871ff84d4d6eb454492910ec1
cpcwood <cpcwood@outlook.com>
2020-10-13 14:31:47 +0100

    add factorybot methods to models/user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     password { 'Securepass1' }
     mobile_number { '+447123456789' }
   end
+
+  factory :user2, class: 'User' do
+    email { 'test2@example.com' }
+    username { 'test2' }
+    password { 'Securepass2' }
+    mobile_number { '+447234567891' }
+  end
 end
