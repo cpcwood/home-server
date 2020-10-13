d35fc64fca052899b773fbec20fe575d30e9aca6
cpcwood <cpcwood@outlook.com>
2020-10-13 14:20:50 +0100

    add factorybot methods to models/site_setting spec

--- spec/models/site_setting_spec.rb
+++ spec/models/site_setting_spec.rb
@@ -12,7 +12,7 @@
 #
 
 RSpec.describe SiteSetting, type: :model do
-  subject { SiteSetting.create(name: 'test_name', typed_header_enabled: false, header_text: 'test header_text', subtitle_text: 'test subtitle_text') }
+  subject { create(:site_setting) }
 
   describe 'name validations' do
     it 'rejects too short' do
