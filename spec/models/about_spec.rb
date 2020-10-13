0b8ebf3daebbf4537cbbb4af32aee861805d19f5
cpcwood <cpcwood@outlook.com>
2020-10-13 14:11:41 +0100

    add factorybot methods to models/about spec

--- spec/models/about_spec.rb
+++ spec/models/about_spec.rb
@@ -10,28 +10,21 @@
 #
 
 RSpec.describe About, type: :model do
-  let(:image_path_valid) { Rails.root.join('spec/files/sample_image.jpg') }
-  let(:image_file_upload) { fixture_file_upload(image_path_valid, 'image/jpg') }
+  let(:image_file_upload) { fixture_file_upload(Rails.root.join('spec/files/sample_image.jpg'), 'image/jpg') }
+  let(:about) { create(:about) }
 
   describe '#change_messages' do
     it 'no changes' do
-      about = About.create
       about.reload
       expect(about.change_messages).to eq([])
     end
 
     it 'attribute changes' do
-      about = About.create
       about.update(name: 'new name', about_me: 'new description')
       expect(about.change_messages).to eq(['Name updated!', 'About me updated!'])
     end
 
     it 'attachment changes' do
-      about = About.create(profile_image_attributes: {
-                             image_file: image_file_upload
-                           })
-      about.reload
-      about.profile_image.reload
       about.update(profile_image_attributes: {
                      image_file: image_file_upload
                    })
