ca5eb5ba6f90fed04b03bc67ab67b11cb7842730
cpcwood <cpcwood@outlook.com>
2020-10-14 18:26:18 +0100

    AdminLinkHelper#admin_link_helper_edit_link complex link

--- app/helpers/admin_link_helper.rb
+++ app/helpers/admin_link_helper.rb
@@ -1,6 +1,6 @@
 module AdminLinkHelper
   def admin_link_helper_edit_link
     current_path = request.original_fullpath
-    current_path.sub(%r{/([\w\-.~]+)$}, '/admin/\1/edit')
+   "/admin#{current_path}/edit"
   end
 end
--- spec/helpers/admin_link_helper_spec.rb
+++ spec/helpers/admin_link_helper_spec.rb
@@ -6,5 +6,12 @@ describe AdminLinkHelper do
       allow(helper).to receive(:request).and_return(request_mock)
       expect(helper.admin_link_helper_edit_link).to eq('/admin/about/edit')
     end
+
+    it 'complex link' do
+      simple_path = '/test/123/about'
+      request_mock = double(:request, original_fullpath: simple_path)
+      allow(helper).to receive(:request).and_return(request_mock)
+      expect(helper.admin_link_helper_edit_link).to eq('/admin/test/123/about/edit')
+    end
   end
 end
