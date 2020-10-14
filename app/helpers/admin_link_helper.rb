c032429c0637671f8d126133f8f967d7cd6ce3c5
cpcwood <cpcwood@outlook.com>
2020-10-14 18:45:43 +0100

    admin toolbar specs

--- app/helpers/admin_link_helper.rb
+++ app/helpers/admin_link_helper.rb
@@ -1,6 +1,6 @@
 module AdminLinkHelper
   def admin_link_helper_edit_link
     current_path = request.original_fullpath
-   "/admin#{current_path}/edit"
+    "/admin#{current_path}/edit"
   end
 end
