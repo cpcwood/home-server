4a6f2764aefb725655e6cc2a3adba9f29a9e02b9
cpcwood <cpcwood@outlook.com>
2020-10-15 14:40:51 +0100

    AdminLinkHelper.on_admin_page? - on admin page spec

--- spec/helpers/admin_link_helper_spec.rb
+++ spec/helpers/admin_link_helper_spec.rb
@@ -14,4 +14,13 @@ describe AdminLinkHelper do
       expect(helper.admin_link_helper_edit_link).to eq('/admin/test/123/about/edit')
     end
   end
+
+  describe '#on_admin_page?' do
+    it 'on admin page' do
+      simple_path = '/admin'
+      request_mock = double(:request, original_fullpath: simple_path)
+      allow(helper).to receive(:request).and_return(request_mock)
+      expect(helper.on_admin_page?).to eq(true)
+    end
+  end
 end
