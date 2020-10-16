2680e580b21e45a1e639dbf2066cca1a72487837
cpcwood <cpcwood@outlook.com>
2020-10-16 12:20:01 +0100

    /admin/about - add index route and empty view

---
+++ spec/feature/admin_update_blog_posts_spec.rb
@@ -0,0 +1,27 @@
+feature 'admin update blog posts', feature: true, slow: true do
+  scenario 'create blog post' do
+    seed_db
+    visit('/blog')
+    expect(page).not_to have_button('Admin Edit')
+    login_feature
+    visit('/blog')
+    click_on('Admin Edit')
+    click_on('Create New')
+    fill_in('post[title]', with: 'post title')
+    fill_in('post[overview]', with: 'post overview')
+    fill_in('post[text]', with: 'post text content')
+    blog_publish_date = DateTime.new(2020, 04, 19, 0, 0, 0)
+    fill_in('post[date_published]', with: blog_publish_date)
+    click_button('Create Blog Post')
+    expect(page).to have_content('New blog post created!')
+    expect(page).to have_content('post section')
+    expect(page).to have_content('post overview')
+    expect(page).to have_content(blog_publish_date)
+    click_on('View Section')
+    expect(page).to have_content('post section')
+    expect(page).to have_content('post overview')
+    expect(page).to have_content(blog_publish_date)
+    # add feature for viewing specific post by css selector.first
+    # add feature for editing specific post by css selector.first
+  end
+end
