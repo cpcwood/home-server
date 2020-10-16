feca592d578b953d4a1797be29ebb30b51a3de6d
cpcwood <cpcwood@outlook.com>
2020-10-16 11:55:45 +0100

    AdminLinkHelper.admin_link_helper_index_link - add method

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
-
-RSpec.describe 'Posts', type: :request do
+Spec.describe 'Posts', type: :request do
+  it 'Renders posts index page' do
+    get '/posts'
+    expect(response).to render_template(:index)
+  end
 end
