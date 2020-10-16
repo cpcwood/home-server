7a6583695c34559e14c687ddbcf442ebfb4d0c52
cpcwood <cpcwood@outlook.com>
2020-10-16 15:46:29 +0100

    POST /admin/posts - save error

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -10,19 +10,35 @@ module Admin
     end
 
     def create
-      @user.posts.create(permitted_params)
-      redirect_to(admin_posts_path, notice: 'New blog post created')        
+      @notices = []
+      @alerts = []
+      begin
+        create_post
+      rescue StandardError => e
+        @alerts.push('Sorry, something went wrong!')
+        @alerts.push(e.message)
+      end
+      redirect_to(admin_posts_path, notice: @notices, alert: @alerts)
     end
 
     private
 
     def permitted_params
       params.require(:post).permit([
-        :title,
-        :overview,
-        :date_published,
-        :text
-      ])
+                                     :title,
+                                     :overview,
+                                     :date_published,
+                                     :text
+                                   ])
+    end
+
+    def create_post
+      new_post = @user.posts.new
+      if new_post.update(permitted_params)
+        @notices.push('New blog post created')
+      else
+        @alerts.push(new_post.errors.values.flatten.last)
+      end
     end
   end
 end
