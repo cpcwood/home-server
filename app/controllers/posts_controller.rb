13e11c2132bbdd46669d6289dc3ba384bc217883
cpcwood <cpcwood@outlook.com>
2020-10-19 18:24:05 +0100

    Posts views - render show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(date_published: :desc)
   end
+
+  def show
+    @post = Post.find_by(id: sanitize(params[:id]))
+  end
+
+  private
+
+  def sanitize(string)
+    ActiveRecord::Base.sanitize_sql(string) unless string.nil?
+  end
 end
