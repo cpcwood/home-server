2cd20c952fe41ceb6caa63d5b4560b31c054d109
cpcwood <cpcwood@outlook.com>
2020-10-19 16:33:55 +0100

    AdminPostsController#update - basic update route

--- spec/requests/admin/posts_request_spec.rb
+++ spec/requests/admin/posts_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Request Admin:Posts', type: :request do
 
   describe 'GET /admin/blog #index' do
     it 'Renders page' do
-      login
+      create(:post, user: @user)
       get '/admin/blog'
       expect(response).to render_template(:index)
     end
@@ -40,6 +40,27 @@ RSpec.describe 'Request Admin:Posts', type: :request do
       allow_any_instance_of(Post).to receive(:save).and_raise('general error')
       post('/admin/posts', params: valid_post_attributes)
       expect(response.body).to include('general error')
+    end
+  end
+
+  describe 'PUT /admin/posts/:id #update' do
+    let(:update_params) do
+      { post: {
+        title: 'updated blog post',
+        overview: 'updated overview',
+        date_published: DateTime.new(2020, 04, 19, 0, 0, 0),
+        text: 'updated post content'
+      }}
+    end
+
+    it 'update sucessful' do
+      post = create(:post, user: @user)
+      put("/admin/posts/#{post.id}", params: update_params)
+      expect(response).to redirect_to(admin_posts_path)
+      expect(flash[:notice]).to include('Blog post updated')
+      post.reload
+      expect(post.title).to eq(update_params[:post][:title])
+      expect(post.overview).to eq(update_params[:post][:overview])
     end
   end
 end
