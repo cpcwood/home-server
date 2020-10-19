43a5ee6d0b78aa2bfb9ef93cfa1ba7f6a537a97b
cpcwood <cpcwood@outlook.com>
2020-10-19 15:49:27 +0100

    AdminPosts - convert post overview view into button to edit path

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     resources :cover_images, only: [:update], path: '/cover-images'
     resource :about, only: [:edit, :update]
     resources :posts, only: [:index], :path => "/blog"
-    resources :posts, only: [:new, :create]
+    resources :posts, only: [:new, :create, :edit]
   end
 
   get '/say-hello', to: 'homepages#index'
