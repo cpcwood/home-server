109166146eaa35e87443e1a2b98874b239076be2
cpcwood <cpcwood@outlook.com>
2020-10-20 17:42:17 +0100

    remove ap from spec

--- spec/helpers/date_helper_spec.rb
+++ spec/helpers/date_helper_spec.rb
@@ -5,7 +5,6 @@ describe DateHelper do
     it 'process to html' do
       result = helper.full_date(date)
       expect(result).to eq('<span>April 19<sup>th</sup>, 2020</span>')
-      ap result.class
       expect(result).to be_kind_of(ActiveSupport::SafeBuffer)
     end
   end
