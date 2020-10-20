e6530770d81c8e7ffd171fad6f7262d88a29e837
cpcwood <cpcwood@outlook.com>
2020-10-20 17:32:15 +0100

    update admin update posts feature test - include new format of date

--- spec/feature/admin_update_blog_posts_spec.rb
+++ spec/feature/admin_update_blog_posts_spec.rb
@@ -13,26 +13,25 @@ feature 'admin update blog posts', feature: true, slow: true do
     fill_in('post[title]', with: 'post title')
     fill_in('post[overview]', with: 'post overview')
     fill_in('post[text]', with: 'post text content')
-    blog_publish_date = DateTime.new(2020, 04, 19, 0, 0, 0)
-    fill_in('post[date_published]', with: blog_publish_date)
+    fill_in('post[date_published]', with: DateTime.new(2020, 04, 19, 0, 0, 0))
     click_button('Submit')
     expect(page).to have_content('Blog post created')
     expect(page).to have_content('post title')
     expect(page).to have_content('post overview')
-    expect(page).to have_content(blog_publish_date.utc)
+    expect(page.html).to have_content('April 19th, 2020')
 
     # view post
     click_on('View Section')
     expect(page).to have_content('post title')
     expect(page).to have_content('post overview')
-    expect(page).to have_content(blog_publish_date.utc)
-    first('.view-blog-post-button').click
+    expect(page.html).to have_content('April 19th, 2020')
+    first('.show-blog-post-button').click
     expect(page).to have_content('post text content')
     click_on('Back to all posts')
 
     # edit post
     click_on('Admin Edit')
-    first('.edit-blog-post-button').click
+    first('.show-blog-post-button').click
     expect(page).to have_content('post text content')
     fill_in('post[title]', with: 'new title')
     click_button('Submit')
@@ -43,6 +42,7 @@ feature 'admin update blog posts', feature: true, slow: true do
 
     # destroy post
     click_on('Admin Edit')
+    first('.show-blog-post-button').click
     first('.destroy-blog-post-button').click
     expect(page).to have_content('Blog post removed')
     expect(page).to have_content('There are no posts here...')
