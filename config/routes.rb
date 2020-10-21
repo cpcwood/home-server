f0ed10b26fc557a28c423be46d6fa9d35e1d1526
cpcwood <cpcwood@outlook.com>
2020-10-21 15:33:03 +0100

    /admin/gallery_images/new - form created

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     resources :posts, only: [:index], :path => "/blog"
     resources :posts, only: [:new, :create, :edit, :update, :destroy]
     resources :gallery_images, only: [:index], :path => "/gallery"
-    resources :gallery_images, only: [:new]
+    resources :gallery_images, only: [:new, :create]
   end
 
   get '/say-hello', to: 'homepages#index'
