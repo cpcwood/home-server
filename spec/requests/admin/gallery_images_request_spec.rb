5072b821f321e3d35be4dcaae83e7ec595322abc
cpcwood <cpcwood@outlook.com>
2020-10-21 15:59:12 +0100

    POST /admin/gallery-images - successful request

--- spec/requests/admin/gallery_images_request_spec.rb
+++ spec/requests/admin/gallery_images_request_spec.rb
@@ -11,15 +11,36 @@ RSpec.describe 'Admin::GalleryImages', type: :request do
     end
   end
 
-  describe 'GET /admin/gallery_images/new' do
+  describe 'GET /admin/gallery-images/new' do
     it 'template render' do
-      get('/admin/gallery_images/new')
+      get('/admin/gallery-images/new')
       expect(response).to render_template(:new)
     end
   end
 
-  describe 'POST /admin/gallery_images' do
+  let(:image_path) { Rails.root.join('spec/files/sample_image.jpg') }
+  let(:image_invalid_path) { Rails.root.join('spec/files/sample_image_invalid.jpg') }
+  let(:image_fixture) { fixture_file_upload(image_path, 'image/png') }
+  let(:image_fixture_invalid) { fixture_file_upload(image_invalid_path, 'image/png') }
+  let(:image_file_error) { double :image_file, attach: false }
+
+  describe 'POST /admin/gallery-images' do
+    let(:valid_attributes) do
+      {
+        gallery_image: {
+          image_file: image_fixture,
+          description: 'new gallery image',
+          date_taken: Date.new(2020, 04, 19),
+          latitude: 180,
+          longitude: -180
+        }
+      }
+    end
+
     it 'successful request' do
+      post('/admin/gallery-images', params: valid_attributes)
+      expect(flash[:notice]).to include('Gallery image created')
+      expect(response).to redirect_to(admin_gallery_images_path)
     end
   end
 end
