dd6bca0fa485af28cede42b61d9c131b50b4f45a
cpcwood <cpcwood@outlook.com>
2020-10-22 13:39:31 +0100

    ProfileImage - update spec to includ default values

--- spec/models/profile_image_spec.rb
+++ spec/models/profile_image_spec.rb
@@ -20,11 +20,26 @@
 #
 
 RSpec.describe ProfileImage, type: :model do
-  describe 'before validation' do
+
+  subject { create(:profile_image) }
+
+  describe '#x_dim' do
+    it 'default_value' do
+      expect(subject.x_dim).to eq(ProfileImage::X_DIM)
+    end
+  end
+
+  describe '#y_dim' do
+    it 'default_value' do
+      expect(subject.y_dim).to eq(ProfileImage::Y_DIM)
+    end
+  end
+
+  describe 'before_validation' do
     describe '#set_defaults' do
       it 'no description' do
         profile_image = create(:profile_image, description: nil)
-        expect(profile_image.description).to_not be_nil
+        expect(profile_image.description).to eq('about-me-profile-image')
       end
     end
   end
