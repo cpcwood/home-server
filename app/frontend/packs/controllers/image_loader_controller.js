d123eeb3203947aacbd6ddc3ad4b45662d1bac81
cpcwood <cpcwood@outlook.com>
2020-10-22 19:21:00 +0100

    JS ImageLoaderController - refactor to use js to add transition delay

--- app/frontend/packs/controllers/image_loader_controller.js
+++ app/frontend/packs/controllers/image_loader_controller.js
@@ -12,7 +12,9 @@ export default class extends Controller {
     this.imageCounter += 1
     if (this.imageCounter === this.targetNumber) {
       for (let i = 0; i < this.fadeTargets.length; i++) {
-        this.fadeTargets[i].classList.add('fade-in')
+        let target = this.fadeTargets[i]
+        target.classList.add('fade-in')
+        target.style["transitionDelay"] = `${i*0.1}s`
       }
     }
   }
@@ -21,7 +23,9 @@ export default class extends Controller {
     this.imageCounter = 0
     this.targetNumber = 0
     for (let i = 0; i < this.fadeTargets.length; i++) {
-      this.fadeTargets[i].classList.remove('fade-in')
+      let target = this.fadeTargets[i]
+      target.classList.remove('fade-in')
+      target.style["transitionDelay"] = '0'
     }
   }
 }
