c025086ba8239c5532e4463491a62c952b1c760a
cpcwood <cpcwood@outlook.com>
2020-10-22 13:37:05 +0100

    HeaderImage spec

---
+++ spec/models/header_image_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe HeaderImage, type: :model do
+
+  subject { create(:header_image) }
+
+  describe '#default_image' do
+    it 'default value' do
+      expect(subject.default_image).to eq('default_images/default_header_image.jpg')
+    end
+  end
+
+  describe '#x_dim' do
+    it 'default_value' do
+      expect(subject.x_dim).to eq(HeaderImage::X_DIM)
+    end
+  end
+
+  describe '#y_dim' do
+    it 'default_value' do
+      expect(subject.y_dim).to eq(HeaderImage::Y_DIM)
+    end
+  end
+end
