7f54d175e94b3259f63b64b99539a634c49f5bec
cpcwood <cpcwood@outlook.com>
2020-10-23 15:36:38 +0100

    ProfileImage - remove redundant attributes

--- spec/models/header_image_spec.rb
+++ spec/models/header_image_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: header_images
+#
+#  id              :bigint           not null, primary key
+#  description     :string
+#  x_loc           :integer          default(50)
+#  y_loc           :integer          default(50)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  site_setting_id :bigint
+#
+# Indexes
+#
+#  index_header_images_on_site_setting_id  (site_setting_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (site_setting_id => site_settings.id)
+#
 RSpec.describe HeaderImage, type: :model do
   subject { create(:header_image) }
 
