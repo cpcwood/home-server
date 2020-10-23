6ceb33239f29f85105ca3b461f6ccd039328d594
cpcwood <cpcwood@outlook.com>
2020-10-23 15:04:11 +0100

    JS GalleryLightboxController - css overrides and options added to config

--- app/frontend/packs/controllers/gallery_lightbox_controller.js
+++ app/frontend/packs/controllers/gallery_lightbox_controller.js
@@ -5,10 +5,15 @@ export default class extends Controller {
   static targets = ['container']
 
   initialize () {
-    this.lightbox = new SimpleLightbox(this.data.get('item-selector'), {})
-  }
-
-  disconnect () {
-
+    this.lightbox = new SimpleLightbox(this.data.get('item-selector'), {
+      showCounter: false,
+      animationSpeed: 100,
+      htmlClass: false,
+      animationSlide: false,
+      history: false,
+      close: false,
+      fadeSpeed: 150,
+      captionClass: 'lightbox-caption'
+    })
   }
 }
