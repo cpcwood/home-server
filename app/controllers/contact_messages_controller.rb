7c97a4b1184d8605eb37bd3014a98f2ff51d3c3f
cpcwood <cpcwood@outlook.com>
2020-10-26 17:35:33 +0000

    POST /contact-messages - save failure

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -5,8 +5,24 @@ class ContactMessagesController < ApplicationController
   end
 
   def create
-    ContactMessage.create(permitted_params)
-    redirect_to(contact_path, notice: 'Message sent! You should receive a confirmation email shortly.')
+    @notices = []
+    @alerts = []
+    @contact_message = ContactMessage.new
+    update_model(model: @contact_message, success_message: 'Message sent! You should receive a confirmation email shortly.')
+    if @alerts.any?
+      flash[:alert] = @alerts
+      render(
+        partial: 'partials/form_replacement',
+        locals: {
+          selector_id: 'contact-messages-new-form',
+          form_partial: 'contact_messages/new_form',
+          model: { contact_message: @contact_message }
+        },
+        formats: [:js])
+      flash[:alert] = nil
+    else
+      redirect_to(contact_path, notice: @notices)
+    end
   end
 
   private
@@ -19,5 +35,13 @@ class ContactMessagesController < ApplicationController
         :email,
         :subject,
         :content)
+  end
+
+  def update_model(model:, success_message:)
+    if model.update(permitted_params)
+      @notices.push(success_message)
+    else
+      @alerts.push(model.errors.values.flatten.last)
+    end
   end
 end
