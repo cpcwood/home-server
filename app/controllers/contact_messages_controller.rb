89fe7c4e691796d1e2dba6dc7a1e5614a8e080de
cpcwood <cpcwood@outlook.com>
2020-10-26 17:21:57 +0000

    POST /contact-messages - successful request

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -3,4 +3,21 @@ class ContactMessagesController < ApplicationController
     @contact_message = ContactMessage.new
     @about = About.first
   end
+
+  def create
+    ContactMessage.create(permitted_params)
+    redirect_to(contact_path, notice: 'Message sent! You should receive a confirmation email shortly.')
+  end
+
+  private
+
+  def permitted_params
+    params
+      .require(:contact_message)
+      .permit(
+        :from,
+        :email,
+        :subject,
+        :content)
+  end
 end
