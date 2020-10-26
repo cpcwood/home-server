88f28fbc4620dda5cc432599ac002503f232fe1b
cpcwood <cpcwood@outlook.com>
2020-10-26 18:43:35 +0000

    NewContactMessageJob - send to About.contact_email

--- app/jobs/new_contact_message_job.rb
+++ app/jobs/new_contact_message_job.rb
@@ -2,6 +2,6 @@ class NewContactMessageJob < ApplicationJob
   queue_as :default
 
   def perform(contact_message:)
-    ContactMessageMailer.with(message: contact_message).send_contact_message.deliver_now
+    ContactMessageMailer.with(message: contact_message, deliver_to: About.first.contact_email).send_contact_message.deliver_now
   end
 end
