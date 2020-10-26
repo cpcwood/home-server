1e845fed0f8075271dffb06078c02b5c539cf2db
cpcwood <cpcwood@outlook.com>
2020-10-26 18:52:33 +0000

    NewContactMessageJob - send confirmation email

--- app/jobs/new_contact_message_job.rb
+++ app/jobs/new_contact_message_job.rb
@@ -3,5 +3,6 @@ class NewContactMessageJob < ApplicationJob
 
   def perform(contact_message:)
     ContactMessageMailer.with(message: contact_message, deliver_to: About.first.contact_email).send_contact_message.deliver_now
+    ContactMessageMailer.with(message: contact_message).send_confirmation.deliver_now
   end
 end
