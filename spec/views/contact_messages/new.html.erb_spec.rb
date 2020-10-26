e5dfe84eb67d89d2521e2a0a6704e1eb07440bb8
cpcwood <cpcwood@outlook.com>
2020-10-26 17:10:50 +0000

    ContactMessage Views - add contact details to contact form

--- spec/views/contact_messages/new.html.erb_spec.rb
+++ spec/views/contact_messages/new.html.erb_spec.rb
@@ -1,10 +1,12 @@
 describe 'Views' do
   let(:contact_message) { build(:contact_message) }
+  let(:about) { build(:about) }
 
   describe '/contact rendering' do
     context 'new view' do
       before(:each) do
         assign(:contact_message, contact_message)
+        assign(:about, about)
         render template: '/contact_messages/new.html.erb'
       end
 
@@ -14,6 +16,11 @@ describe 'Views' do
         expect(rendered).to match('contact_message_email')
         expect(rendered).to match('contact_message_content')
         expect(rendered).to match(contact_messages_path)
+      end
+
+      it 'it renders about details' do
+        expect(rendered).to match(about.name)
+        expect(rendered).to match(about.location)
       end
     end
   end
