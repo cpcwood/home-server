646d1fc2448ccb833e3434869ac3d3307922225c
cpcwood <cpcwood@outlook.com>
2020-10-26 18:52:33 +0000

    NewContactMessageJob - send confirmation email

--- spec/jobs/new_contact_message_job_spec.rb
+++ spec/jobs/new_contact_message_job_spec.rb
@@ -1,11 +1,14 @@
 RSpec.describe NewContactMessageJob, type: :job do
   let(:about) {create(:about)}
   let(:contact_message) { build_stubbed(:contact_message) }
+  let(:mock_contact_message_mailer) {double(:ContactMessageMailer, send_confirmation: nil, send_contact_message: nil, deliver_now: nil)}
 
   it 'Contact message sent' do
-    mock_contact_message_mailer = double(:ContactMessageMailer, deliver_now: nil)
-    expect(ContactMessageMailer).to receive(:with).with(message: contact_message, deliver_to: about.contact_email).and_return(mock_contact_message_mailer)
+    about
+    expect(ContactMessageMailer).to receive(:with).twice.and_return(mock_contact_message_mailer)
+    expect(mock_contact_message_mailer).to receive(:send_confirmation).and_return(mock_contact_message_mailer)
     expect(mock_contact_message_mailer).to receive(:send_contact_message).and_return(mock_contact_message_mailer)
+
     NewContactMessageJob.perform_now(contact_message: contact_message)
   end
 end
