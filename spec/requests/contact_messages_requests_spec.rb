9612eb0ad8d3daeca26193ba30a70041974f2457
cpcwood <cpcwood@outlook.com>
2020-10-26 17:35:33 +0000

    POST /contact-messages - save failure

--- spec/requests/contact_messages_requests_spec.rb
+++ spec/requests/contact_messages_requests_spec.rb
@@ -3,15 +3,23 @@ RSpec.describe 'ContactMessages', type: :request do
     seed_user_and_settings
   end
 
-  let(:valid_params) {
+  let(:valid_params) do
     { contact_message: {
-        from: 'test user',
-        email: 'test@example.com',
-        subject: 'new message subject',
-        content: 'new message text content'
-      }
-    }
-  }
+      from: 'test user',
+      email: 'test@example.com',
+      subject: 'new message subject',
+      content: 'new message text content'
+    }}
+  end
+
+  let(:invalid_params) do
+    { contact_message: {
+      from: '',
+      email: 'test@example.com',
+      subject: 'new message subject',
+      content: 'new message text content'
+    }}
+  end
 
   describe 'GET /contact' do
     it 'sucessful request' do
@@ -26,6 +34,12 @@ RSpec.describe 'ContactMessages', type: :request do
       expect(response).to redirect_to(contact_path)
       expect(flash[:notice]).to include('Message sent! You should receive a confirmation email shortly.')
       # expect mailerjobs to be made
+    end
+
+    it 'save failure' do
+      expect{ post('/contact-messages', params: invalid_params) }.not_to(change{ ContactMessage.all.length })
+      expect(response).not_to redirect_to(admin_posts_path)
+      expect(response.body).to include('From field cannot be blank')
     end
   end
 end
