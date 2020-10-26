18caf55bd6119fe2890eaed0ed8dfbd49eba7455
cpcwood <cpcwood@outlook.com>
2020-10-26 19:02:39 +0000

    ContactMessageMailer.contact_message - deliver to user

--- app/jobs/new_contact_message_job.rb
+++ app/jobs/new_contact_message_job.rb
@@ -2,7 +2,7 @@ class NewContactMessageJob < ApplicationJob
   queue_as :default
 
   def perform(contact_message:)
-    ContactMessageMailer.with(message: contact_message, deliver_to: About.first.contact_email).send_contact_message.deliver_now
-    ContactMessageMailer.with(message: contact_message).send_confirmation.deliver_now
+    ContactMessageMailer.with(message: contact_message, deliver_to: About.first.contact_email).contact_message.deliver_now
+    ContactMessageMailer.with(message: contact_message).confirmation.deliver_now
   end
 end
--- spec/mailers/contact_message_mailer_spec.rb
+++ spec/mailers/contact_message_mailer_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe ContactMessageMailer, type: :mailer do
+  let(:about) {build_stubbed(:about)}
   let(:user) { build_stubbed(:user) }
   let(:contact_message) { build_stubbed(:contact_message, user: user) }
   let(:header_image) { create(:header_image, site_setting: create(:site_setting)) }
@@ -9,7 +10,15 @@ RSpec.describe ContactMessageMailer, type: :mailer do
     header_image
   end
 
-  describe '#send_contact_message' do
-    let(:mail) { ContactMessageMailer.with(contact_message: contact_message).send_contact_message }
+  describe '#contact_message' do
+    let(:mail) { ContactMessageMailer.with(contact_message: contact_message, deliver_to: about.contact_email).contact_message }
+
+    it 'Renders the receivers email' do
+      expect(mail.to).to eql([about.contact_email])
+    end
+
+    it 'Renders the sender email correctly' do
+      expect(mail.from).to eql([Rails.application.credentials.email[:no_reply_email]])
+    end
   end
 end
