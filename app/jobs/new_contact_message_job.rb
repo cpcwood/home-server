516b40bb11a5efe1db51cf7a67ab1bd42b847cdf
cpcwood <cpcwood@outlook.com>
2020-10-26 19:30:05 +0000

    ContactMessageMailer.contact_message - assigns greeting

--- app/jobs/new_contact_message_job.rb
+++ app/jobs/new_contact_message_job.rb
@@ -2,7 +2,7 @@ class NewContactMessageJob < ApplicationJob
   queue_as :default
 
   def perform(contact_message:)
-    ContactMessageMailer.with(message: contact_message, deliver_to: About.first.contact_email).contact_message.deliver_now
+    ContactMessageMailer.with(message: contact_message).contact_message.deliver_now
     ContactMessageMailer.with(message: contact_message).confirmation.deliver_now
   end
 end
--- spec/mailers/contact_message_mailer_spec.rb
+++ spec/mailers/contact_message_mailer_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe ContactMessageMailer, type: :mailer do
-  let(:about) { build_stubbed(:about) }
+  let(:about) { create(:about) }
   let(:user) { build_stubbed(:user) }
   let(:contact_message) { build_stubbed(:contact_message, user: user) }
   let(:header_image) { create(:header_image, site_setting: create(:site_setting)) }
@@ -7,11 +7,13 @@ RSpec.describe ContactMessageMailer, type: :mailer do
   let(:image_attachment) { fixture_file_upload(Rails.root.join("spec/files/#{image_name}"), 'image/png') }
 
   before(:each) do
+    about
+    user
     header_image
   end
 
   describe '#contact_message' do
-    let(:mail) { ContactMessageMailer.with(contact_message: contact_message, deliver_to: about.contact_email).contact_message }
+    let(:mail) { ContactMessageMailer.with(contact_message: contact_message).contact_message }
 
     it 'Renders the receivers email' do
       expect(mail.to).to eql([about.contact_email])
@@ -27,6 +29,10 @@ RSpec.describe ContactMessageMailer, type: :mailer do
 
     it 'Renders the subject' do
       expect(mail.subject).to eql("New contact message: #{contact_message.subject}")
+    end
+
+    it 'Assigns greeting in email' do
+      expect(mail.body.encoded).to match(/Hi[\w\W]+#{about.name}/)
     end
   end
 
