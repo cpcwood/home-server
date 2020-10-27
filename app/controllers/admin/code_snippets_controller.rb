33b328d2ee6e4e6bd858467d9b65fc95d67ec25f
cpcwood <cpcwood@outlook.com>
2020-10-27 14:55:42 +0000

    GET /admin/code-snippets/new - basic route and view

--- app/controllers/admin/code_snippets_controller.rb
+++ app/controllers/admin/code_snippets_controller.rb
@@ -1,5 +1,11 @@
 module Admin
   class CodeSnippetsController < ApplicationController
-    def index; end
+    def index
+      @code_snippets = CodeSnippet.order(created_at: :desc)
+    end
+
+    def new
+      @code_snippet = CodeSnippet.new
+    end
   end
 end
