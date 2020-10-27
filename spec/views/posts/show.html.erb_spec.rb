fcf9ffe42c52bb1d98194acc6af3f249e97b6cd9
cpcwood <cpcwood@outlook.com>
2020-10-27 14:40:58 +0000

    View /code-snippets/:id - render show

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ describe 'Views' do
   let(:user) { build_stubbed(:user) }
   let(:post) { build_stubbed(:post, user: user) }
 
-  describe '/blog rendering' do
+  describe '/blog/:id rendering' do
     it 'show view' do
       assign(:post, post)
       expect_any_instance_of(MarkdownHelper).to receive(:markdown_admin).with(post.text).and_return('some markdown')
