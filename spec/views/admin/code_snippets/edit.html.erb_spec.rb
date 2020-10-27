a07216a8ad08af1433646b26fff6510800b8e128
cpcwood <cpcwood@outlook.com>
2020-10-27 15:55:39 +0000

    View /admin/code-snippets/:id/edit - renders view with model

---
+++ spec/views/admin/code_snippets/edit.html.erb_spec.rb
@@ -0,0 +1,22 @@
+describe 'Views' do
+  let(:code_snippet) { create(:code_snippet) }
+
+  describe '/admin/code_snippets/:id/edit rendering' do
+    context 'edit view' do
+      before(:each) do
+        assign(:code_snippet, code_snippet)
+        render template: '/admin/code_snippets/edit.html.erb'
+      end
+
+      it 'it renders code_snippets' do
+        expect(rendered).to match(code_snippet.title)
+        expect(rendered).to match(code_snippet.overview)
+        expect(rendered).to match(code_snippet.text)
+        expect(rendered).to match(admin_code_snippet_path(code_snippet))
+        expect(rendered).to match(admin_code_snippets_path)
+        expect(rendered).to match('Return')
+        expect(rendered).to match('Remove')
+      end
+    end
+  end
+end
