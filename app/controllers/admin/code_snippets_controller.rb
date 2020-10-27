033e75de6e17b5d4c1ca3e54de975998a4340848
cpcwood <cpcwood@outlook.com>
2020-10-27 15:16:22 +0000

    add code snippets link to admin dashboard sidebar

--- app/controllers/admin/code_snippets_controller.rb
+++ app/controllers/admin/code_snippets_controller.rb
@@ -2,10 +2,12 @@ module Admin
   class CodeSnippetsController < ApplicationController
     def index
       @code_snippets = CodeSnippet.order(created_at: :desc)
+      render layout: 'layouts/admin_dashboard'
     end
 
     def new
       @code_snippet = CodeSnippet.new
+      render layout: 'layouts/admin_dashboard'
     end
   end
 end
