fb5c54ff0382765fa25bc47350dfb41569cffb0c
cpcwood <cpcwood@outlook.com>
2020-10-29 15:21:08 +0000

    View /projects - overview updated to include links to external sites if not blank

--- spec/views/admin/projects/index.html.erb_spec.rb
+++ spec/views/admin/projects/index.html.erb_spec.rb
@@ -16,6 +16,10 @@ describe 'Views' do
       expect(rendered).to match(project2.title)
       expect(rendered).to match(project1.overview)
       expect(rendered).to match(project2.overview)
+      expect(rendered).to match(project1.github_link)
+      expect(rendered).to match(project2.github_link)
+      expect(rendered).to match(project1.site_link)
+      expect(rendered).to match(project2.site_link)
       expect(rendered).to match('toolbar-container')
     end
 
