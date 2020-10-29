e7c949f204f45d22f8dc6e493a537ea34ce9d3f5
cpcwood <cpcwood@outlook.com>
2020-10-29 22:07:00 +0000

    migration to remove snippet and extension from projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,11 +4,9 @@
 #
 #  id          :bigint           not null, primary key
 #  date        :datetime         not null
-#  extension   :string
 #  github_link :string
 #  overview    :text
 #  site_link   :string
-#  snippet     :text
 #  title       :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_215956) do
+ActiveRecord::Schema.define(version: 2020_10_29_220401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,10 +146,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_215956) do
     t.datetime "date", null: false
     t.string "github_link"
     t.string "site_link"
-    t.text "snippet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "extension"
   end
 
   create_table "site_settings", force: :cascade do |t|
