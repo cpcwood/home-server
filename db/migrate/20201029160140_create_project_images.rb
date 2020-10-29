e65d2b19298e739faa2a6a165419e13da06889a2
cpcwood <cpcwood@outlook.com>
2020-10-29 17:20:42 +0000

    Project renders nested attributes for main-project-image in new and update form

--- db/migrate/20201029160140_create_project_images.rb
+++ db/migrate/20201029160140_create_project_images.rb
@@ -1,7 +1,7 @@
 class CreateProjectImages < ActiveRecord::Migration[6.0]
   def change
     create_table :project_images do |t|
-      t.string :description, null: false, default: 'project_image'
+      t.string :description, null: false, default: 'project-image'
       t.string :title
       t.references :project, index: true, foreign_key: true, null: false
       t.timestamps
