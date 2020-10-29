d34df276d04a37d73d9f19068698f71eba36810c
cpcwood <cpcwood@outlook.com>
2020-10-29 15:00:44 +0000

    POST /admin/projects - base controller w/o image upload

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -9,5 +9,55 @@ module Admin
       @project = Project.new
       render layout: 'layouts/admin_dashboard'
     end
+
+    def create
+      @notices = []
+      @alerts = []
+      begin
+        @project = Project.new
+        update_model(model: @project, success_message: 'Project created')
+      rescue StandardError => e
+        logger.error("RESCUE: #{caller_locations.first}\nERROR: #{e}\nTRACE: #{e.backtrace.first}")
+        @alerts.push('Sorry, something went wrong!')
+        @alerts.push(e.message)
+      end
+      if @alerts.any?
+        flash[:alert] = @alerts
+        render(
+          partial: 'partials/form_replacement',
+          locals: {
+            selector_id: 'admin-projects-new-form',
+            form_partial: 'admin/projects/new_form',
+            model: { project: @project }
+          },
+          formats: [:js])
+        flash[:alert] = nil
+      else
+        redirect_to(admin_projects_path, notice: @notices)
+      end
+    end
+
+    private
+
+    def permitted_params
+      params
+        .require(:project)
+        .permit(
+          :title,
+          :overview,
+          :date,
+          :github_link,
+          :site_link,
+          :snippet,
+          :extension)
+    end
+
+    def update_model(model:, success_message:)
+      if model.update(permitted_params)
+        @notices.push(success_message)
+      else
+        @alerts.push(model.errors.values.flatten.last)
+      end
+    end
   end
 end
