ed37c28f45d2ba3612a5d15c3cb3b2bfe6bf2d7f
cpcwood <cpcwood@outlook.com>
2020-12-07 17:50:51 +0000

    kube-autodeploy script update container version find and replace

--- scripts/travis-ci/travis-ci-kube-autodeploy.sh
+++ scripts/travis-ci/travis-ci-kube-autodeploy.sh
@@ -2,27 +2,24 @@
 # TravisCI Autodeploy to Kubernetes Script
 
 # Ensure all required environment variables are present
-if [ -z "$DOCKER_IMAGE_NAME" ] || \
-    [ -z "$KUBERNETES_CLUSTER_CERTIFICATE" ] || \
+if [ -z "$KUBERNETES_CLUSTER_CERTIFICATE" ] || \
     [ -z "$KUBERNETES_SERVER" ] || \
     [ -z "$KUBERNETES_SERVICE_ACC_TOKEN" ]; then
-    printenv
     >&2 echo 'Required variable unset, docker build and deploy failed'
     exit 1
 fi
 
 # Generate latest docker image name
-full_docker_image_name="$DOCKER_IMAGE_NAME:$(git rev-parse --short=6 HEAD)"
-echo "The application container build is $full_docker_image_name"
+container_version=$(git rev-parse --short=8 HEAD)
 
 echo : "
 Travis-CI autodeploy to kubernetes script
 Repo: $TRAVIS_REPO_SLUG
-Image: $full_docker_image_name
+Image Version: $container_version
 "
 
 # Update kube deployment files with new container version
-find ./kube/ -type f | xargs sed -i "s/CONTAINER_VERSION/$full_docker_image_name/g"
+find ./kube/ -type f | xargs sed -i "s/CONTAINER_VERSION/$container_version/g"
 
 # Install kubernetes cli and add to path
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
