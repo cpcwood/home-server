87802f213c34e5a4dbd9be04b1a538a613084893
cpcwood <cpcwood@outlook.com>
2020-12-07 12:04:49 +0000

    fix substitution error

--- scripts/travis-ci-merge-script.sh
+++ scripts/travis-ci-merge-script.sh
@@ -33,22 +33,20 @@ cd $repo_temp
 printf 'Checking out %s\n' "$TRAVIS_BRANCH"
 git checkout "$TRAVIS_BRANCH"
 
-# Ensure checkout commit is the same as the tested PR commit
-current_commit="${git rev-parse --short HEAD}"
-if [ "$current_commit" != "$TRAVIS_PULL_REQUEST_SHA"]; then
-    >&2 echo "Pull request commit '$TRAVIS_PULL_REQUEST_SHA' and merge '$current_commit' commit do not match, automerge failed"
+# Ensure PR commit is head of branch to be merged
+branch_head_commit=$(git rev-parse HEAD)
+if [ $branch_head_commit != $TRAVIS_PULL_REQUEST_SHA]; then
+    >&2 echo "Pull request commit ($TRAVIS_PULL_REQUEST_SHA) and does not match HEAD of branch to be merged ($branch_head_commit), automerge failed"
     exit 1
 fi
 
->&2 echo "Pull request commit '$TRAVIS_PULL_REQUEST_SHA' and merge '$current_commit' commit do not match, automerge failed"
+printf 'Merging %s\n' "$TRAVIS_PULL_REQUEST_BRANCH"
+git merge "$TRAVIS_PULL_REQUEST_SHA"
 
-# printf 'Merging %s\n' "$TRAVIS_PULL_REQUEST_BRANCH"
-# git merge "$TRAVIS_PULL_REQUEST_SHA"
+printf 'Pushing to %s\n' "$TRAVIS_REPO_SLUG"
+push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$TRAVIS_REPO_SLUG"
 
-# printf 'Pushing to %s\n' "$TRAVIS_REPO_SLUG"
-# push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$TRAVIS_REPO_SLUG"
-
-# # Push to github to complete merge
-# # Redirect to /dev/null to avoid secret leakage
-# git push "$push_uri" "$TRAVIS_BRANCH" >/dev/null 2>&1
-# git push "$push_uri" :"$TRAVIS_PULL_REQUEST_BRANCH" >/dev/null 2>&1
+# Push to github to complete merge
+# Redirect to /dev/null to avoid secret leakage
+git push "$push_uri" "$TRAVIS_BRANCH" >/dev/null 2>&1
+git push "$push_uri" :"$TRAVIS_PULL_REQUEST_BRANCH" >/dev/null 2>&1
