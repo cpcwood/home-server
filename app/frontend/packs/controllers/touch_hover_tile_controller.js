9f0faed7c845577eeaa652ec4529388b57f19faa
cpcwood <cpcwood@outlook.com>
2020-12-10 13:00:28 +0000

    update JS TouchHoverTileController to handle mouse over and mouse leave events

--- app/frontend/packs/controllers/touch_hover_tile_controller.js
+++ app/frontend/packs/controllers/touch_hover_tile_controller.js
@@ -3,23 +3,33 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['coverTitle']
 
-  checkTouch () {
+  touchStart () {
     this.touchTime = new Date(Date.now())
   }
 
-  addHover (event) {
+  touchEnd (event) {
     const diff = new Date(Date.now()) - this.touchTime
     if (diff < 250) {
-      if (!this.coverTitleTarget.classList.contains('hover')) {
-        const otherHover = document.querySelector('.hover')
-        if (otherHover) otherHover.classList.remove('hover')
-        this.coverTitleTarget.classList.add('hover')
-        event.preventDefault()
-      }
+      this.applyHoverToTarget(event)
     }
   }
 
-  disconnect () {
+  applyHoverToTarget (event) {
+    if (!this.coverTitleTarget.classList.contains('hover')) {
+      const otherHoverTiles = document.getElementsByClassName('hover')
+      for (let i = 0; i < otherHoverTiles.length; i += 1) {
+        otherHoverTiles[i].classList.remove('hover')
+      }
+      this.coverTitleTarget.classList.add('hover')
+      event.preventDefault()
+    }
+  }
+
+  teardown () {
     this.coverTitleTarget.classList.remove('hover')
+  }
+
+  disconnect () {
+    this.teardown()
   }
 }
