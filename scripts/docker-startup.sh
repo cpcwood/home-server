427931665af48e0699895ebc5040372037320563
cpcwood <cpcwood@outlook.com>
2020-12-15 00:22:42 +0000

    add base cron task for site map update

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
 bundle exec rails sitemap:refresh:no_ping
+bundle exec rails whenever --update-crontab
 bundle exec rails server -b 0.0.0.0 -p 5000
