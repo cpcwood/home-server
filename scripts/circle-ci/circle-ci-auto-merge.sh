55c94c2a07dc04239c12f9cde73bb7f24e195672
cpcwood <cpcwood@outlook.com>
2020-12-16 00:28:00 +0000

    update circle-ci automerge script to extract PR number and use github API to merge request

--- scripts/circle-ci/circle-ci-auto-merge.sh
+++ scripts/circle-ci/circle-ci-auto-merge.sh
@@ -1,62 +1,51 @@
 #!/bin/bash -e
-# CircleCI Automerge GitHub PR Script
+# CircleCI Automerge GitHub Pull Request Script
 
-if [ -z "${CIRCLE_PULL_REQUEST}" ]; then
+# Ensure in PR
+if [ -z "$CIRCLE_PULL_REQUEST" ]; then
     >&2 echo 'Not in pull request, skipping automerge'
     exit 1
 fi
 
 # Ensure all required environment variables are present
 if [ -z "$CIRCLE_PROJECT_REPONAME" ] || \
+    [ -z "$CIRCLE_PROJECT_USERNAME" ] || \
+    [ -z "$CIRCLE_PULL_REQUEST" ] || \
     [ -z "$CIRCLE_BRANCH" ] || \
     [ -z "$CIRCLE_SHA1" ] || \
-    [ -z "$GITHUB_SECRET_TOKEN" ] || \
-    [ -z "$GIT_COMMITTER_EMAIL" ] || \
-    [ -z "$GIT_COMMITTER_NAME" ]; then
+    [ -z "$GITHUB_SECRET_TOKEN" ]; then
     >&2 echo 'Required variable unset, automerging failed'
     exit 1
 fi
 
-# Fetch target branch
-if [ -n ${CIRCLE_PR_NUMBER} ]; then
-    curl -L "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -o jq
-    chmod +x jq
-    url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_SECRET_TOKEN"
-    target_branch=$(curl "$url" | ./jq '.base.ref' | tr -d '"')
-fi
-
-echo : "
-Travis-CI automerge pull request script
-Repo: $CIRCLE_PROJECT_REPONAME 
-Merging: $CIRCLE_BRANCH >> $target_branch 
-"
-
-# Clean repo
-git clean
-
-# # Checkout full repo
-# repo_temp=$(mktemp -d)
-# git clone "https://github.com/$TRAVIS_REPO_SLUG" "$repo_temp"
-# cd $repo_temp
-
-printf 'Checking out %s\n' "$target_branch"
-git checkout "$target_branch"
-
-
-# Ensure PR commit is head of branch to be merged
-branch_head_commit=$(git rev-parse HEAD)
-if [ $branch_head_commit != $CIRCLE_SHA1]; then
-    >&2 echo "Pull request commit ($CIRCLE_SHA1) and does not match HEAD of branch to be merged ($branch_head_commit), automerge failed"
+# Extract GitHub PR number
+github_pr_number="$(echo "$CIRCLE_PULL_REQUEST" | sed -n 's/^.*\/\([0-9]\+\)$/\1/p')"
+if [ -z "$github_pr_number" ] || \
+    >&2 echo 'GitHub PR number not found'
     exit 1
 fi
 
-printf 'Merging %s\n' "$CIRCLE_BRANCH"
-git merge "$CIRCLE_SHA1"
+# Fetch target branch name
+curl -L "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -o jq
+chmod +x jq
+url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$github_pr_number"
+target_branch=$(curl -H "Authorization: $GITHUB_SECRET_TOKEN" "$url" | ./jq '.base.ref' | tr -d '"')
+if [ -z "$target_branch" ] || \
+    >&2 echo 'Failed to fetch GitHub PR target branch'
+    exit 1
+fi
 
-printf 'Pushing to %s\n' "$CIRCLE_PROJECT_REPONAME"
-push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$CIRCLE_PROJECT_REPONAME"
+echo : "
+CircleCI Automerge Pull Request
+Repo: $CIRCLE_PROJECT_REPONAME
+Pull Request: $github_pr_number
+Merging: $CIRCLE_BRANCH >> $target_branch 
+"
 
-# Push to github to complete merge
-# Redirect to /dev/null to avoid secret leakage
-git push "$push_uri" "$target_branch" >/dev/null 2>&1
-git push "$push_uri" :"$CIRCLE_BRANCH" >/dev/null 2>&1
+# Merge PR via GitHub API
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: $GITHUB_SECRET_TOKEN" \
+  "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$github_pr_number/merge" \
+  -d '{"commit_title":"CircleCI automerge '"$CIRCLE_BRANCH >> $target_branch"'", "sha": "'"$CIRCLE_SHA1"'"}'
