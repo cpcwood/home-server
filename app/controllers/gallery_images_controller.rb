2483728c5d7698e8ec980dc60caea67f3fb3d5b6
cpcwood <cpcwood@outlook.com>
2021-01-22 23:11:22 +0000

    add base json format reponse

--- app/controllers/gallery_images_controller.rb
+++ app/controllers/gallery_images_controller.rb
@@ -1,5 +1,19 @@
 class GalleryImagesController < ApplicationController
+
+  PER_PAGE = 12
+
   def index
-    @gallery_images = GalleryImage.order(date_taken: :desc)
+    @gallery_images = GalleryImage.order(date_taken: :desc).limit(PER_PAGE).offset(calc_offset)
+    respond_to do |format|
+      format.html
+      format.json  { render :json => 'test' }
+    end
+  end
+
+  private
+
+  def calc_offset
+    page_number = params['page'].to_i
+    page_number > 0 ? ((page_number - 1) * PER_PAGE) : 0
   end
 end
