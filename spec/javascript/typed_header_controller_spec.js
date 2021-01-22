6c5b064847ce3e08c84a30e22af5612853075662
cpcwood <cpcwood@outlook.com>
2021-01-22 19:02:38 +0000

    add cookie to typed header to prevent retyping on same visit

--- spec/javascript/typed_header_controller_spec.js
+++ spec/javascript/typed_header_controller_spec.js
@@ -1,31 +1,35 @@
 import { Application } from 'stimulus'
 import Typed from 'typed.js'
+import Cookies from 'js-cookie'
 import typedHeaderController from 'controllers/typed_header_controller'
 jest.mock('typed.js')
+jest.mock('js-cookie')
 
 describe('typed_header_controller', () => {
   let application
 
+  const testHtml = `
+  <div id="container">
+    <div id="controllerDiv" data-controller="typed-header">
+      <div id="typed-strings-header">
+        <p data-typed-header-target="typedHeaderText"> 
+          Typed header
+        </p>
+      </div>
+      <span id="typed-header" data-typed-header-target="typedHeader"></span>
+      <div id="typed-strings-subtitle">
+        <p data-typed-header-target="typedSubtitleText"> 
+          Typed subtitle 
+        </p>
+      </div>
+      <span id="typed-subtitle" data-typed-header-target="typedSubtitle"></span>
+    </div>
+  </div>
+  `
+
   beforeAll(() => {
     application = Application.start()
     application.register('typed-header', typedHeaderController)
-  })
-
-  beforeEach(() => {
-    document.body.innerHTML = `
-    <div id="container">
-      <div id="controllerDiv" data-controller="typed-header">
-        <div id="typed-strings-header">
-          <p> Typed header </p>
-        </div>
-        <span id="typed-header" data-typed-header-target="typedHeader"></span>
-        <div id="typed-strings-subtitle">
-          <p> Typed subtitle </p>
-        </div>
-        <span id="typed-subtitle" data-typed-header-target="typedSubtitle"></span>
-      </div>
-    </div>
-    `
   })
 
   afterEach(() => {
@@ -36,19 +40,40 @@ describe('typed_header_controller', () => {
     application.unload('typed-header')
   })
 
-  describe('#connect', () => {
-    it('typed object initialized', () => {
-      expect(Typed).toHaveBeenCalledTimes(1)
+  describe('first visit', () => {
+    beforeEach(() => {
+      document.body.innerHTML = testHtml
+    })
+
+    describe('#connect', () => {
+      it('typed object initialized', () => {
+        expect(Typed).toHaveBeenCalledTimes(1)
+        expect(Cookies.set).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    describe('#disconnect', () => {
+      beforeEach(() => {
+        document.body.innerHTML = 'test'
+      })
+
+      it('typed object removed', () => {
+        expect(Typed.mock.instances[0].destroy).toHaveBeenCalledTimes(1)
+      })
     })
   })
 
-  describe('#disconnect', () => {
+  describe('second visit', () => {
     beforeEach(() => {
-      document.body.innerHTML = 'test'
+      Cookies.get.mockImplementation(() => {
+        return 'true'
+      })
+      document.body.innerHTML = testHtml
     })
 
-    it('typed object removed', () => {
-      expect(Typed.mock.instances[0].destroy).toHaveBeenCalledTimes(1)
+    it('strings filled if already typed', () => {
+      expect(Typed).toHaveBeenCalledTimes(0)
+      expect(Cookies.set).toHaveBeenCalledTimes(1)
     })
   })
 })
