929f68ba332cb10915bfcecf84c0f0699d8cfd15
cpcwood <cpcwood@outlook.com>
2021-01-23 17:22:52 +0000

    GET /gallery.json - returns json response of image data

--- app/controllers/gallery_images_controller.rb
+++ app/controllers/gallery_images_controller.rb
@@ -1,12 +1,11 @@
 class GalleryImagesController < ApplicationController
-
   PER_PAGE = 12
 
   def index
-    @gallery_images = GalleryImage.order(date_taken: :desc).limit(PER_PAGE).offset(calc_offset)
+    @gallery_images = GalleryImage.order(date_taken: :desc).includes(image_file_attachment: :blob).limit(PER_PAGE).offset(calc_offset)
     respond_to do |format|
       format.html
-      format.json  { render :json => 'test' }
+      format.json { render json: GalleryImageSerializer.new(@gallery_images, {}).serializable_hash }
     end
   end
 
--- spec/helpers/attachment_helper_spec.rb
+++ spec/helpers/attachment_helper_spec.rb
@@ -1,6 +1,8 @@
 describe AttachmentHelper do
   default_image_path = 'path-to-default-image'
-  let(:mock_image_file) { double :image_file }
+  let(:mock_variant) { double :variant, key: 'test-variant' }
+  let(:mock_image_variant) { double :image_file_variant, blob: mock_image_file, variation: mock_variant }
+  let(:mock_image_file) { double :image_file, signed_id: 'test-id', filename: 'test-name' }
   let(:header_image) { double :header_image, image_file: mock_image_file, default_image: default_image_path }
   let(:profile_image) { double :profile_image, image_file: mock_image_file }
 
@@ -74,6 +76,34 @@ describe AttachmentHelper do
     it 'image attached' do
       allow(mock_image_file).to receive(:attached?).and_return(true)
       expect(helper.image_attached?(header_image)).to eq(true)
+    end
+  end
+
+  describe '#fetch_image_url' do
+    it 'no image passed' do
+      expect(helper.fetch_image_url(image_model: nil)).to eq(AttachmentHelper::IMAGE_NOT_FOUND)
+    end
+
+    it 'no image attached' do
+      allow(mock_image_file).to receive(:attached?).and_return(false)
+      expect(helper.fetch_image_url(image_model: profile_image)).to eq(AttachmentHelper::IMAGE_NOT_FOUND)
+    end
+
+    it 'default image method implemented' do
+      allow(mock_image_file).to receive(:attached?).and_return(false)
+      expect(helper.fetch_image_url(image_model: header_image)).to eq(default_image_path)
+    end
+
+    it 'image attached' do
+      allow(mock_image_file).to receive(:attached?).and_return(true)
+      expect(helper.fetch_image_url(image_model: header_image)).to eq('/rails/active_storage/blobs/test-id/test-name')
+    end
+
+    it 'variant passed' do
+      allow(mock_image_file).to receive(:attached?).and_return(true)
+      allow(header_image).to receive(:variant_sizes).and_return({ thumbnail: { resize_to_limit: [100, 100] }})
+      allow(mock_image_file).to receive(:variant).with({ resize_to_limit: [100, 100] }).and_return(mock_image_variant)
+      expect(helper.fetch_image_url(image_model: header_image, variant: :thumbnail)).to eq('/rails/active_storage/representations/test-id/test-variant/test-name')
     end
   end
 end
