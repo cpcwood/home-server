0cfacda010a6acbb11f9fe9fbd253625cbee9992
cpcwood <cpcwood@outlook.com>
2021-02-16 23:57:09 +0000

    update gallery admin tooling

--- spec/views/partials/_admin_toolbar_spec.rb
+++ spec/views/partials/_admin_toolbar_spec.rb
@@ -1,12 +1,13 @@
 describe 'Views' do
   let(:user) { create(:user) }
   let(:new_model) { build(:post, user: user) }
-  let(:exisiting_model) { create(:about) }
+  let(:exisiting_singular_resource) { create(:about) }
+  let(:exisiting_resources) { create(:post, user: user) }
 
   describe 'admin toolbar rendering' do
     context('user unassigned') do
       it 'no render' do
-        render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_model }
+        render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_resources }
         expect(rendered).not_to match('toolbar-container')
       end
     end
@@ -19,7 +20,12 @@ describe 'Views' do
 
       context 'admin scope' do
         it 'singular model' do
-          render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_model }
+          render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_singular_resource, singular: true }
+          expect(rendered).to match('View Section')
+        end
+
+        it 'resources' do
+          render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_resources }
           expect(rendered).to match('View Section')
         end
       end
@@ -35,7 +41,7 @@ describe 'Views' do
         end
 
         it 'edit model' do
-          render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_model }
+          render partial: 'partials/admin_toolbar.html.erb', locals: { model: exisiting_resources }
           expect(rendered).to match('Edit')
         end
       end
