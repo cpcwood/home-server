6b6c9969e356cdff8031d38fab3138c8c4c7e7dd
cpcwood <cpcwood@outlook.com>
2021-03-14 18:10:03 +0000

    issue: confusing directory structure for docker and scripts. fix: move rearrange docker, kube, and scripts folders

--- scripts/docker/docker-startup-app.dev.sh
+++ scripts/docker/docker-startup-app.dev.sh
@@ -2,5 +2,14 @@
 bundle install --quiet
 kill -INT $(cat ./tmp/pids/server.pid) > /dev/null 2>&1
 rm -f ./tmp/pids/server.pid > /dev/null 2>&1
-bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
+
+
+if bundle exec rails db:exists ; then
+  bundle exec rails db:migrate
+else
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
+fi
+
 bundle exec rails server -b 0.0.0.0 -p 5000
