78f9e5102b5014b93e056a2497a33696d5b2c349
cpcwood <cpcwood@outlook.com>
2021-03-14 21:26:29 +0000

    update sentry error reporting frequency

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,13 +3,6 @@ if Rails.env.production?
     config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = [:active_support_logger]
 
-    # Set tracesSampleRate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production
-    config.traces_sample_rate = 0.5
-    # or
-    config.traces_sampler = lambda do |context|
-      true
-    end
+    config.traces_sample_rate = 1
   end
 end
