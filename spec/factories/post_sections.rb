290b34b82f087da32174dd26fad653249a637f8e
cpcwood <cpcwood@outlook.com>
2021-04-04 17:29:54 +0100

    add default order to post section

--- spec/factories/post_sections.rb
+++ spec/factories/post_sections.rb
@@ -3,7 +3,7 @@
 # Table name: post_sections
 #
 #  id         :bigint           not null, primary key
-#  order      :integer          not null
+#  order      :integer          default(0), not null
 #  text       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
