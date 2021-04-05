20012509fcde85ecf0451b36c78b8a530bc0bf44
cpcwood <cpcwood@outlook.com>
2021-04-05 16:11:52 +0100

    admin posts controller accepts post section images uploads and destroys

--- db/migrate/20210405135744_add_post_section_image_to_post_section.rb
+++
@@ -1,5 +0,0 @@
-class AddPostSectionImageToPostSection < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :post_sections, :post_section_image, index: true, foreign_key: true
-  end
-end
