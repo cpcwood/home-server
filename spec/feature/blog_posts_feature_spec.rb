66f32f8f8940256bc11ed86aa59e93679cd9fc0b
cpcwood <cpcwood@outlook.com>
2021-04-05 01:26:35 +0100

    feature test blog post editor js controller

--- spec/feature/blog_posts_feature_spec.rb
+++ spec/feature/blog_posts_feature_spec.rb
@@ -28,17 +28,38 @@ feature 'blog posts feature', feature: true do
       login_feature
     end
 
-    scenario 'create blog post' do
+    scenario 'create blog post', js: true do
       visit('/blog')
       click_on('Create New')
       fill_in('post[title]', with: 'post title')
       fill_in('post[overview]', with: 'post overview')
-      fill_in('post[post_sections_attributes][0][text]', with: 'post text content')
+
+      # fill in first post section
+      fill_in('post[post_sections_attributes][0][text]', with: 'post section text content 1')
+
+      # add second post section
+      first('.post-editor-toolbar-button.new-section').click
+      fill_in('post[post_sections_attributes][1][text]', with: 'post section text content 2')
+
+      # add third post section
+      all('.post-editor-toolbar-button.new-section')[1].click
+      fill_in('post[post_sections_attributes][2][text]', with: 'post section text content 3')
+
+      # remove second post section
+      all('.post-editor-toolbar-button.remove-section')[1].click
+
+      # remove and restore third post section
+      all('.post-editor-toolbar-button.remove-section')[1].click
+      all('.post-editor-toolbar-button.restore-section')[1].click
+
       fill_in('post[date_published]', with: DateTime.new(2020, 04, 19, 0, 0, 0))
-      click_button('Submit')
+      first('.input-submit').click
       expect(page).to have_content('Blog post created')
       expect(page).to have_content('post title')
       expect(page).to have_content('April 19th, 2020')
+      expect(page).to have_content('post section text content 1')
+      expect(page).to have_content('post section text content 3')
+      expect(page).not_to have_content('post section text content 2')
       first('.reading-footer .standard-button').click
       expect(page).to have_content('post overview')
     end
