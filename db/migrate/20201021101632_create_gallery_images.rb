43c42aa510c4dfe04d6b441b5befd06e28b0399a
cpcwood <contact@cpcwood.com>
2022-07-30 21:42:17 +0100

    Fix migrations

--- db/migrate/20201021101632_create_gallery_images.rb
+++ db/migrate/20201021101632_create_gallery_images.rb
@@ -3,8 +3,8 @@ class CreateGalleryImages < ActiveRecord::Migration[6.0]
     create_table :gallery_images do |t|
       t.string :description, null: false
       t.timestamp :date_taken
-      t.decimal :latitude, { precision: 10, scale: 6 }
-      t.decimal :longitude, { precision: 10, scale: 6 }
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
       t.references :user, index: true, foreign_key: true, null: false
       t.timestamps
     end
--- db/migrate/20210404162559_add_default_to_post_section_order.rb
+++ db/migrate/20210404162559_add_default_to_post_section_order.rb
@@ -1,5 +1,5 @@
 class AddDefaultToPostSectionOrder < ActiveRecord::Migration[6.1]
   def change
-    change_column :post_sections, :order, :integer, { null: false, default: 0 }
+    change_column :post_sections, :order, :integer, null: false, default: 0
   end
 end
