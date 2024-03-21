2fedb7b65101474ed3a52707c5d53fe1ba239c49
Chris Wood <chris.wood@thebeansgroup.com>
2024-03-21 23:27:40 +0000

    fix bug where gallery images page renders duplicate image

--- app/controllers/gallery_images_controller.rb
+++ app/controllers/gallery_images_controller.rb
@@ -2,7 +2,7 @@ class GalleryImagesController < ApplicationController
   PAGE_SIZE = 12
 
   def index
-    @gallery_images = GalleryImage.order(date_taken: :desc).includes(image_file_attachment: :blob).limit(PAGE_SIZE).offset(calc_offset)
+    @gallery_images = GalleryImage.order(date_taken: :desc, id: :desc).includes(image_file_attachment: :blob).limit(PAGE_SIZE).offset(calc_offset)
     respond_to do |format|
       format.html
       format.json { render json: GalleryImageSerializer.new(@gallery_images, {}).serializable_hash }
