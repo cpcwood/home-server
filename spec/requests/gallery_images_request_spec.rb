71a8af0a868de3ec2543c1fe4fe17ff7c8cb001b
Chris Wood <chris.wood@thebeansgroup.com>
2024-03-22 00:38:59 +0000

    rubocop linting

--- spec/requests/gallery_images_request_spec.rb
+++ spec/requests/gallery_images_request_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'GalleryImagesController', type: :request do
       it 'json response' do
         allow_any_instance_of(AttachmentHelper).to receive(:fetch_image_url).and_return('test')
         get('/gallery.json')
-        parsed_body = JSON.parse(response.body)
+        parsed_body = response.parsed_body
         expect(parsed_body['data'][0]['attributes']).to eq({
                                                              'title' => @gallery_image.title,
                                                              'description' => @gallery_image.description,
@@ -32,10 +32,10 @@ RSpec.describe 'GalleryImagesController', type: :request do
         seed_images.times{ create(:gallery_image, user: @user) }
         seeded_images = 1 + seed_images
         get('/gallery.json')
-        parsed_body = JSON.parse(response.body)
+        parsed_body = response.parsed_body
         expect(parsed_body['data'].length).to eq([GalleryImagesController::PAGE_SIZE, seeded_images].min)
         get('/gallery.json?page=2')
-        parsed_body = JSON.parse(response.body)
+        parsed_body = response.parsed_body
         expect(parsed_body['data'].length).to eq([seeded_images - GalleryImagesController::PAGE_SIZE, 0].max)
       end
     end
