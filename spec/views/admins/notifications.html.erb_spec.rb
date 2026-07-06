fa70aa7c4ba1a2fe782c8aedb4a56aadb38f5c37
cpcwood <contact@cpcwood.com>
2026-07-06 13:08:26 +0100

    app: remove back buttons and dead notification placeholders

--- spec/views/admins/notifications.html.erb_spec.rb
+++ spec/views/admins/notifications.html.erb_spec.rb
@@ -10,32 +10,5 @@ describe 'Views' do
 
       expect(rendered).to match(/Contact Emails/)
     end
-
-    it 'Displays new blog comment notifications' do
-      # Placeholder spec
-      assign(:user, user)
-
-      render template: 'admins/notifications'
-
-      expect(rendered).to match(/Blog Comments/)
-    end
-
-    it 'Displays new blog comment notifications' do
-      # Placeholder spec
-      assign(:user, user)
-
-      render template: 'admins/notifications'
-
-      expect(rendered).to match(/Blog Comments/)
-    end
-
-    it 'Displays new say hello notifications' do
-      # Placeholder spec
-      assign(:user, user)
-
-      render template: 'admins/notifications'
-
-      expect(rendered).to match(/Say Hellos/)
-    end
   end
 end
